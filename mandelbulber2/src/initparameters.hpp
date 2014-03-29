7d7132890350be3c757eaedb47d8d0fa78ce4f1b
buddhi1980@gmail.com <buddhi1980@gmail.com@d4d486b1-51a2-e619-c922-7372ee5d08d3>
2014-03-29 15:37:54 +0000

    ...added two missing files

---
+++ mandelbulber2/src/initparameters.hpp
@@ -0,0 +1,15 @@
+/*
+ * initparameters.hpp
+ *
+ *  Created on: Mar 29, 2014
+ *      Author: krzysztof
+ */
+
+#ifndef INITPARAMETERS_HPP_
+#define INITPARAMETERS_HPP_
+
+
+void InitParams(parameters::container *par);
+
+
+#endif /* INITPARAMETERS_HPP_ */
