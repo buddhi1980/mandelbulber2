820345ecac0d0fd46790ac34e77bb33819fbd1f4
jenzebas@gmail.com <jenzebas@gmail.com@d4d486b1-51a2-e619-c922-7372ee5d08d3>
2014-11-14 17:00:44 +0000

    Added crossplatform consistent random number generator based on the gsl library. See the README for new dependencies.

---
+++ trunk/mandelbulber2/src/random.cpp
@@ -0,0 +1,47 @@
+/**
+ * Mandelbulber v2, a 3D fractal generator
+ *
+ * cRandom class - crossplatform deterministic random generator based on GSL library
+ *
+ * Copyright (C) 2014 Krzysztof Marczak
+ *
+ * This file is part of Mandelbulber.
+ *
+ * Mandelbulber is free software: you can redistribute it and/or modify it under the terms of the
+ * GNU General Public License as published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * Mandelbulber is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ *
+ * See the GNU General Public License for more details. You should have received a copy of the GNU
+ * General Public License along with Mandelbulber. If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Authors: Sebastian Jennen (jenzebas@gmail.com)
+ */
+
+#include "random.hpp"
+
+cRandom::cRandom()
+{
+	gBaseRand = gsl_rng_alloc(gsl_rng_mt19937);
+}
+
+void cRandom::Initialize(int seed)
+{
+	gsl_rng_set(gBaseRand, seed);
+}
+
+
+int cRandom::Random(int max)
+{
+	gsl_rng_uniform_int(gBaseRand, max + 1);
+}
+
+double cRandom::Random(double min, double max, double resolution)
+{
+	double range = max - min;
+	int n = round(range / resolution);
+	return min + resolution * Random(n);
+}
+
---
+++ trunk/mandelbulber2/src/random.hpp
@@ -0,0 +1,42 @@
+/**
+ * Mandelbulber v2, a 3D fractal generator
+ *
+ * cRandom class - crossplatform deterministic random generator based on GSL library
+ *
+ * Copyright (C) 2014 Krzysztof Marczak
+ *
+ * This file is part of Mandelbulber.
+ *
+ * Mandelbulber is free software: you can redistribute it and/or modify it under the terms of the
+ * GNU General Public License as published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * Mandelbulber is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ *
+ * See the GNU General Public License for more details. You should have received a copy of the GNU
+ * General Public License along with Mandelbulber. If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Authors: Sebastian Jennen (jenzebas@gmail.com)
+ */
+
+#ifndef RANDOM_HPP_
+#define RANDOM_HPP_
+
+#include <math.h>
+#include <gsl/gsl_rng.h>
+#include <gsl/gsl_randist.h>
+
+class cRandom
+{
+
+public:
+	cRandom();
+	void Initialize(int seed);
+	int Random(int max);
+	double Random(double min, double max, double resolution);
+private:
+	const gsl_rng *gBaseRand;
+};
+
+#endif /* RANDOM_HPP_ */
