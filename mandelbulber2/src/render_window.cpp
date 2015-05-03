ff3017800dd85c24fc8b88c915b0685ab00318c3
buddhi1980 <buddhi1980@gmail.com>
2015-05-03 21:55:58 +0200

    Updated info files for release 2.04

--- mandelbulber2/src/render_window.cpp
+++ mandelbulber2/src/render_window.cpp
@@ -662,7 +662,8 @@ void RenderWindow::slotMenuAboutMandelbulber()
 	text += "Licence: GNU GPL version 3.0\n";
 	text += "Copyright Ⓒ 2014\n";
 	text += "project leader: Krzysztof Marczak\n";
-	text += "commiters:\n";
+	text += "programmers:\n";
+	text +=	"Krzysztof Marczak\n";
 	text +=	"Sebastian Jennen\n";
 	text += "\n";
 	text += "Thanks to many friends from www.fractalforums.com for help\n";
