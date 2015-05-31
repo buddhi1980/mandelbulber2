fba2ab3f93c455b05e09524fa44d98e2b40dd823
buddhi1980 <buddhi1980@gmail.com>
2015-05-31 17:34:16 +0200

    Added missing files

---
+++ mandelbulber2/src/statistics.cpp
@@ -0,0 +1,23 @@
+/*
+ * statistics.cpp
+ *
+ *  Created on: 31-05-2015
+ *      Author: krzysztof
+ */
+
+#include "statistics.h"
+
+cStatistics::cStatistics()
+{
+	// TODO Auto-generated constructor stub
+  totalNumberOfIterations = 0;
+  missedDE = 0;
+  numberOfRaymarchings = 0;
+  numberOfRenderedPixels = 0;
+}
+
+cStatistics::~cStatistics()
+{
+	// TODO Auto-generated destructor stub
+}
+
---
+++ mandelbulber2/src/statistics.h
@@ -0,0 +1,31 @@
+/*
+ * statistics.h
+ *
+ *  Created on: 31-05-2015
+ *      Author: krzysztof
+ */
+
+#ifndef MANDELBULBER2_SRC_STATISTICS_H_
+#define MANDELBULBER2_SRC_STATISTICS_H_
+
+#include "histogram.hpp"
+class cStatistics
+{
+public:
+	cStatistics();
+	~cStatistics();
+	cHistogram histogramIterations;
+	cHistogram histogramStepCount;
+	long long totalNumberOfIterations;
+	int missedDE;
+	int numberOfRaymarchings;
+	int numberOfRenderedPixels;
+	double time;
+
+	double GetTotalNumberOfIterations() {return totalNumberOfIterations;}
+	double GetNumberOfIterationsPerPixel() {return (double)totalNumberOfIterations / numberOfRenderedPixels;}
+	double GetNumberOfIterationsPerSecond() {return (double)totalNumberOfIterations / time;}
+	double GetMissedDEPercentage() {return (double)missedDE / numberOfRaymarchings * 100.0;}
+};
+
+#endif /* MANDELBULBER2_SRC_STATISTICS_H_ */
