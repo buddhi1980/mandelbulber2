3403018855b3196122f0013ec5c9f4def1cf2385
Sebastian Jennen <sebastian.jennen@gmx.de>
2015-09-20 16:21:53 +0200

    added missing GPL headers in new files

--- mandelbulber2/src/command_line_interface.cpp
+++ mandelbulber2/src/command_line_interface.cpp
@@ -1,8 +1,23 @@
-/*
- * cCommandLineInterface.cpp
+/**
+ * Mandelbulber v2, a 3D fractal generator
  *
- *  Created on: 11-09-2015
- *      Author: krzysztof
+ * cCommandLineInterface - CLI Input handler
+ *
+ * Copyright (C) 2014 Krzysztof Marczak
+ *
+ * This file is part of Mandelbulber.
+ *
+ * Mandelbulber is free software: you can redistribute it and/or modify it under the terms of the
+ * GNU General Public License clias published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * Mandelbulber is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ *
+ * See the GNU General Public License for more details. You should have received a copy of the GNU
+ * General Public License along with Mandelbulber. If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Authors: Krzysztof Marczak (buddhi1980@gmail.com), Sebastian Jennen
  */
 
 #include "command_line_interface.hpp"
--- mandelbulber2/src/rendering_configuration.cpp
+++ mandelbulber2/src/rendering_configuration.cpp
@@ -1,8 +1,23 @@
-/*
- * rendering_configuration.cpp
+/**
+ * Mandelbulber v2, a 3D fractal generator
  *
- *  Created on: 13 wrz 2015
- *      Author: krzysztof
+ * cRenderingConfiguration - holds configuration for the render engine
+ *
+ * Copyright (C) 2014 Krzysztof Marczak
+ *
+ * This file is part of Mandelbulber.
+ *
+ * Mandelbulber is free software: you can redistribute it and/or modify it under the terms of the
+ * GNU General Public License as published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * Mandelbulber is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ *
+ * See the GNU General Public License for more details. You should have received a copy of the GNU
+ * General Public License along with Mandelbulber. If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Authors: Krzysztof Marczak (buddhi1980@gmail.com)
  */
 
 #include "rendering_configuration.hpp"
