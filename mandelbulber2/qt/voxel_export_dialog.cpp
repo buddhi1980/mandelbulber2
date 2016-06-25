6c8e060580aea4c40dd592d9ff84dc8306cbc719
zebastian <jenzebas@gmail.com>
2016-06-25 18:59:06 +0200

    FIX voxel_export_dialog.cpp wrong finished() free-ing
    voxel_export_dialog.cpp threw an error, on render finished:
    Warning: QThread: Destroyed while thread is still running (:0,
    This was caused by freeing the thread before voxelExport was freed.
    Now freeing is analogue to freeing of renderworker, which fixes this
    issue (maybe there were no bad side effects with this issue)

--- mandelbulber2/qt/voxel_export_dialog.cpp
+++ mandelbulber2/qt/voxel_export_dialog.cpp
@@ -1,15 +1,45 @@
-#include "voxel_export_dialog.h"
-#include "ui_voxel_export_dialog.h"
-#include <QtCore>
-#include <QFileDialog>
-#include <QMessageBox>
-#include "../src/interface.hpp"
-#include "../src/initparameters.hpp"
-#include "../src/error_message.hpp"
+/**
+ * Mandelbulber v2, a 3D fractal generator       ,=#MKNmMMKmmßMNWy,
+ *                                             ,B" ]L,,p%%%,,,§;, "K
+ * Copyright (C) 2014 Krzysztof Marczak        §R-==%w["'~5]m%=L.=~5N
+ *                                        ,=mm=§M ]=4 yJKA"/-Nsaj  "Bw,==,,
+ * This file is part of Mandelbulber.    §R.r= jw",M  Km .mM  FW ",§=ß., ,TN
+ *                                     ,4R =%["w[N=7]J '"5=],""]]M,w,-; T=]M
+ * Mandelbulber is free software:     §R.ß~-Q/M=,=5"v"]=Qf,'§"M= =,M.§ Rz]M"Kw
+ * you can redistribute it and/or     §w "xDY.J ' -"m=====WeC=\ ""%""y=%"]"" §
+ * modify it under the terms of the    "§M=M =D=4"N #"%==A%p M§ M6  R' #"=~.4M
+ * GNU General Public License as        §W =, ][T"]C  §  § '§ e===~ U  !§[Z ]N
+ * published by the                    4M",,Jm=,"=e~  §  §  j]]""N  BmM"py=ßM
+ * Free Software Foundation,          ]§ T,M=& 'YmMMpM9MMM%=w=,,=MT]M m§;'§,
+ * either version 3 of the License,    TWw [.j"5=~N[=§%=%W,T ]R,"=="Y[LFT ]N
+ * or (at your option)                   TW=,-#"%=;[  =Q:["V""  ],,M.m == ]N
+ * any later version.                      J§"mr"] ,=,," =="""J]= M"M"]==ß"
+ *                                          §= "=C=4 §"eM "=B:m\4"]#F,§~
+ * Mandelbulber is distributed in            "9w=,,]w em%wJ '"~" ,=,,ß"
+ * the hope that it will be useful,                 . "K=  ,=RMMMßM"""
+ * but WITHOUT ANY WARRANTY;                            .'''
+ * without even the implied warranty
+ * of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ *
+ * See the GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License
+ * along with Mandelbulber. If not, see <http://www.gnu.org/licenses/>.
+ *
+ * ###########################################################################
+ *
+ * Authors: Sebastian Jennen (sebastian.jennen@gmx.de)
+ *
+ * cVoxelExportDialog class - class for handling the voxelexport dialog
+ */
 
-cVoxelExportDialog::cVoxelExportDialog(QWidget *parent) :
-	QDialog(parent),
-	ui(new Ui::cVoxelExportDialog)
+#include "voxel_export_dialog.h"
+#include "../src/error_message.hpp"
+#include "../src/initparameters.hpp"
+#include "../src/interface.hpp"
+#include "ui_voxel_export_dialog.h"
+
+cVoxelExportDialog::cVoxelExportDialog(QWidget *parent)
+		: QDialog(parent), ui(new Ui::cVoxelExportDialog)
 {
 	initFinished = false;
 	slicerBusy = false;
@@ -23,18 +53,18 @@ cVoxelExportDialog::cVoxelExportDialog(QWidget *parent) :
 
 cVoxelExportDialog::~cVoxelExportDialog()
 {
-  delete ui;
+	delete ui;
 }
 
 void cVoxelExportDialog::on_pushButton_start_render_layers_clicked()
 {
-	if(!slicerBusy)
+	if (!slicerBusy)
 	{
 		SynchronizeInterfaceWindow(this, gPar, qInterface::read);
 		slicerBusy = true;
 		CVector3 limitMin;
 		CVector3 limitMax;
-		if(gPar->Get<bool>("voxel_custom_limit_enabled"))
+		if (gPar->Get<bool>("voxel_custom_limit_enabled"))
 		{
 			limitMin = gPar->Get<CVector3>("voxel_limit_min");
 			limitMax = gPar->Get<CVector3>("voxel_limit_max");
@@ -49,34 +79,37 @@ void cVoxelExportDialog::on_pushButton_start_render_layers_clicked()
 		int samplesX = gPar->Get<int>("voxel_samples_x");
 		int samplesY = gPar->Get<int>("voxel_samples_y");
 		int samplesZ = gPar->Get<int>("voxel_samples_z");
-		voxelExport = new cVoxelExport(samplesX, samplesY, samplesZ, limitMin, limitMax, folder, maxIter);
+		voxelExport =
+			new cVoxelExport(samplesX, samplesY, samplesZ, limitMin, limitMax, folder, maxIter);
 		QObject::connect(voxelExport,
-			SIGNAL(updateProgressAndStatus(const QString&, const QString&, double)),
-			this,
-			SLOT(slotUpdateProgressAndStatus(const QString&, const QString&, double)));
+			SIGNAL(updateProgressAndStatus(const QString &, const QString &, double)), this,
+			SLOT(slotUpdateProgressAndStatus(const QString &, const QString &, double)));
 
-		QThread *thread = new QThread; //deleted by deleteLater()
+		QThread *thread = new QThread; // deleted by deleteLater()
 		voxelExport->moveToThread(thread);
 		QObject::connect(thread, SIGNAL(started()), voxelExport, SLOT(ProcessVolume()));
 		thread->setObjectName("Slicer");
 		thread->start();
-		QObject::connect(voxelExport, SIGNAL(finished()), thread, SLOT(deleteLater()));
-		QObject::connect(voxelExport, SIGNAL(finished()), voxelExport, SLOT(deleteLater()));
 		QObject::connect(voxelExport, SIGNAL(finished()), this, SLOT(slotSlicerFinished()));
+		QObject::connect(voxelExport, SIGNAL(finished()), voxelExport, SLOT(deleteLater()));
+		QObject::connect(voxelExport, SIGNAL(finished()), thread, SLOT(quit()));
+		QObject::connect(thread, SIGNAL(finished()), thread, SLOT(deleteLater()));
 
 		ui->pushButton_start_render_layers->setEnabled(false);
 		ui->pushButton_stop_render_layers->setEnabled(true);
 	}
 	else
 	{
-		cErrorMessage::showMessage(QObject::tr("Voxel Export is busy. Stop unfinished rendering before starting new voxel export job."),
+		cErrorMessage::showMessage(
+			QObject::tr(
+				"Voxel Export is busy. Stop unfinished rendering before starting new voxel export job."),
 			cErrorMessage::errorMessage);
 	}
 }
 
 void cVoxelExportDialog::on_pushButton_stop_render_layers_clicked()
 {
-	if(slicerBusy)
+	if (slicerBusy)
 	{
 		voxelExport->Stop();
 	}
@@ -85,14 +118,10 @@ void cVoxelExportDialog::on_pushButton_stop_render_layers_clicked()
 void cVoxelExportDialog::on_pushButton_select_image_path_clicked()
 {
 	QString dir = QDir::toNativeSeparators(
-		QFileDialog::getExistingDirectory(
-			this,
-			tr("Select path for slice images"),
-			QDir::toNativeSeparators(ui->text_voxel_image_path->text())
-		)
-	);
+		QFileDialog::getExistingDirectory(this, tr("Select path for slice images"),
+			QDir::toNativeSeparators(ui->text_voxel_image_path->text())));
 
-	if(dir.length() > 0)
+	if (dir.length() > 0)
 	{
 		ui->text_voxel_image_path->setText(dir);
 	}
@@ -102,7 +131,7 @@ void cVoxelExportDialog::on_pushButton_show_layers_clicked()
 {
 	WriteLog("Prepare PlayerWidget class", 2);
 	SynchronizeInterfaceWindow(this, gPar, qInterface::read);
-	if(!gMainInterface->imageSequencePlayer)
+	if (!gMainInterface->imageSequencePlayer)
 	{
 		gMainInterface->imageSequencePlayer = new PlayerWidget;
 	}
@@ -120,17 +149,17 @@ void cVoxelExportDialog::slotSlicerFinished()
 	ui->pushButton_stop_render_layers->setEnabled(false);
 }
 
-void cVoxelExportDialog::slotUpdateProgressAndStatus(const QString &text,
-		const QString &progressText, double progress)
+void cVoxelExportDialog::slotUpdateProgressAndStatus(
+	const QString &text, const QString &progressText, double progress)
 {
 	ui->label_info->setText(text);
 	if (!ui->progressBar->isVisible()) ui->progressBar->setVisible(true);
-	ui->progressBar->setValue(progress * 1000.0);
+	ui->progressBar->setValue((int)(progress * 1000.0));
 	ui->progressBar->setTextVisible(true);
 	ui->progressBar->setFormat(progressText);
 }
 
-void cVoxelExportDialog::closeEvent(QCloseEvent * event)
+void cVoxelExportDialog::closeEvent(QCloseEvent *event)
 {
 	if (!slicerBusy)
 	{
@@ -138,14 +167,16 @@ void cVoxelExportDialog::closeEvent(QCloseEvent * event)
 	}
 	else
 	{
-		cErrorMessage::showMessage(QObject::tr("Voxel Export is busy. Stop unfinished rendering before closing the voxel export dialog."),
+		cErrorMessage::showMessage(
+			QObject::tr(
+				"Voxel Export is busy. Stop unfinished rendering before closing the voxel export dialog."),
 			cErrorMessage::errorMessage);
 		event->ignore();
 	}
 }
 
-void cVoxelExportDialog::showEvent(QShowEvent * event)
+void cVoxelExportDialog::showEvent(QShowEvent *event)
 {
-	(void) event;
+	(void)event;
 	SynchronizeInterfaceWindow(this, gPar, qInterface::write);
 }
--- mandelbulber2/qt/voxel_export_dialog.h
+++ mandelbulber2/qt/voxel_export_dialog.h
@@ -1,13 +1,48 @@
+/**
+ * Mandelbulber v2, a 3D fractal generator       ,=#MKNmMMKmmßMNWy,
+ *                                             ,B" ]L,,p%%%,,,§;, "K
+ * Copyright (C) 2014 Krzysztof Marczak        §R-==%w["'~5]m%=L.=~5N
+ *                                        ,=mm=§M ]=4 yJKA"/-Nsaj  "Bw,==,,
+ * This file is part of Mandelbulber.    §R.r= jw",M  Km .mM  FW ",§=ß., ,TN
+ *                                     ,4R =%["w[N=7]J '"5=],""]]M,w,-; T=]M
+ * Mandelbulber is free software:     §R.ß~-Q/M=,=5"v"]=Qf,'§"M= =,M.§ Rz]M"Kw
+ * you can redistribute it and/or     §w "xDY.J ' -"m=====WeC=\ ""%""y=%"]"" §
+ * modify it under the terms of the    "§M=M =D=4"N #"%==A%p M§ M6  R' #"=~.4M
+ * GNU General Public License as        §W =, ][T"]C  §  § '§ e===~ U  !§[Z ]N
+ * published by the                    4M",,Jm=,"=e~  §  §  j]]""N  BmM"py=ßM
+ * Free Software Foundation,          ]§ T,M=& 'YmMMpM9MMM%=w=,,=MT]M m§;'§,
+ * either version 3 of the License,    TWw [.j"5=~N[=§%=%W,T ]R,"=="Y[LFT ]N
+ * or (at your option)                   TW=,-#"%=;[  =Q:["V""  ],,M.m == ]N
+ * any later version.                      J§"mr"] ,=,," =="""J]= M"M"]==ß"
+ *                                          §= "=C=4 §"eM "=B:m\4"]#F,§~
+ * Mandelbulber is distributed in            "9w=,,]w em%wJ '"~" ,=,,ß"
+ * the hope that it will be useful,                 . "K=  ,=RMMMßM"""
+ * but WITHOUT ANY WARRANTY;                            .'''
+ * without even the implied warranty
+ * of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ *
+ * See the GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License
+ * along with Mandelbulber. If not, see <http://www.gnu.org/licenses/>.
+ *
+ * ###########################################################################
+ *
+ * Authors: Sebastian Jennen (sebastian.jennen@gmx.de)
+ *
+ * cVoxelExportDialog class - class for handling the voxelexport dialog
+ */
+
 #ifndef VOXEL_EXPORT_DIALOG_H
 #define VOXEL_EXPORT_DIALOG_H
 
-#include <QDialog>
+#include "../src/automated_widgets.hpp"
 #include "../src/global_data.hpp"
 #include "../src/voxel_export.hpp"
-#include "../src/automated_widgets.hpp"
+#include <QDialog>
 
-namespace Ui {
-	class cVoxelExportDialog;
+namespace Ui
+{
+class cVoxelExportDialog;
 }
 
 class cVoxelExportDialog : public QDialog
@@ -23,19 +58,20 @@ public slots:
 	void on_pushButton_stop_render_layers_clicked();
 	void on_pushButton_select_image_path_clicked();
 	void on_pushButton_show_layers_clicked();
-	void slotUpdateProgressAndStatus(const QString &text,
-		const QString &progressText, double progress);
+	void slotUpdateProgressAndStatus(
+		const QString &text, const QString &progressText, double progress);
 	void slotSlicerFinished();
 
 protected:
-	void closeEvent(QCloseEvent * event);
-	void showEvent(QShowEvent * event);
+	void closeEvent(QCloseEvent *event);
+	void showEvent(QShowEvent *event);
+
 private:
 	Ui::cVoxelExportDialog *ui;
 	cAutomatedWidgets *automatedWidgets;
 	bool initFinished;
 	bool slicerBusy;
-	cVoxelExport* voxelExport;
+	cVoxelExport *voxelExport;
 };
 
 #endif // VOXEL_EXPORT_DIALOG_H
