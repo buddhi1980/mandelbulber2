22ea43307ef153bd82097cbd1516e0962d453089
zebastian <jenzebas@gmail.com>
2016-07-12 00:13:02 +0200

    more changes to code_style_check.php
    is now able to parse old source header

--- mandelbulber2/deploy/code_style_check.php
+++ mandelbulber2/deploy/code_style_check.php
@@ -12,19 +12,30 @@
 define('PROJECT_PATH', realpath(dirname(__FILE__)) . '/../');
 
 $filesToCheckSource = array();
-// $filesToCheckSource[] = PROJECT_PATH . "src/*.cpp";
-// $filesToCheckSource[] = PROJECT_PATH . "src/*.c";
-$filesToCheckSource[] = PROJECT_PATH . "qt/*.cpp";
-$filesToCheckSource[] = PROJECT_PATH . "qt/*.c";
+$filesToCheckSource[] = PROJECT_PATH . "src/*.cpp";
+$filesToCheckSource[] = PROJECT_PATH . "src/*.c";
+// $filesToCheckSource[] = PROJECT_PATH . "qt/*.cpp";
+// $filesToCheckSource[] = PROJECT_PATH . "qt/*.c";
 
 $filesToCheckHeader = array();
-// $filesToCheckHeader[] = PROJECT_PATH . "src/*.hpp";
-// $filesToCheckHeader[] = PROJECT_PATH . "src/*.h";
-$filesToCheckHeader[] = PROJECT_PATH . "qt/*.hpp";
-$filesToCheckHeader[] = PROJECT_PATH . "qt/*.h";
+$filesToCheckHeader[] = PROJECT_PATH . "src/*.hpp";
+$filesToCheckHeader[] = PROJECT_PATH . "src/*.h";
+// $filesToCheckHeader[] = PROJECT_PATH . "qt/*.hpp";
+// $filesToCheckHeader[] = PROJECT_PATH . "qt/*.h";
 
 $sourceFiles = glob("{" . implode(",", $filesToCheckSource) . "}", GLOB_BRACE);
 $headerFiles = glob("{" . implode(",", $filesToCheckHeader) . "}", GLOB_BRACE);
+
+foreach($sourceFiles as $sourceFilePath) {
+	$sourceFileName = basename($sourceFilePath);
+	echo 'handling file: ' . $sourceFileName;
+	$sourceContent = file_get_contents($sourceFilePath);
+	if(!checkFileHeader($sourceContent)) continue;
+	if(!isDryRun()){
+		file_put_contents($sourceFilePath, $sourceContent);	
+	}
+	echo successString(' -> All well') . PHP_EOL;
+}
 
 foreach($headerFiles as $headerFilePath) {
 	$headerFileName = basename($headerFilePath);
@@ -37,6 +48,7 @@ foreach($headerFiles as $headerFilePath) {
 	if(!isDryRun()){
 		file_put_contents($headerFilePath, $headerContent);	
 	}
+	echo successString(' -> All well') . PHP_EOL;
 }
 
 if(isDryRun()){
@@ -58,7 +70,16 @@ function checkFileHeader(&$fileContent){
 		$fileHeader = $matchHeader[1];
 		// $fileSourceCode = $matchHeader[2];
 		if(strpos($fileHeader, 'MKNmMMKmm') === false){
-			echo errorString('header unknown!') . PHP_EOL;
+			$regexOldHeader = '/^[\s\S]+Mandelbulber\sv2.*[\s\S]*?(\w.*)[\S\s]+General\sPublic[\S\s]+Authors:\s(.*)[\s\S]*?\*\/([\s\S]*)$/';
+			if(preg_match($regexOldHeader, $fileContent, $matchHeaderOld)){
+				echo noticeString('header is old, will rewrite to new!') . PHP_EOL;
+				$newFileContent = getFileHeader($matchHeaderOld[2], $matchHeaderOld[1]) . $matchHeaderOld[3];		
+				$fileContent = $newFileContent;
+				return true;
+			}
+			else{
+				echo errorString('header unknown!') . PHP_EOL;
+			}
 		}
 		else{
 			return true;	
@@ -78,15 +99,12 @@ function checkDefines(&$fileContent, $headerFilePath, $headerFileName, $folderNa
 
 		if($newFileContent != $fileContent){
 			$fileContent = $newFileContent;
-			echo noticeString('makro changed') . PHP_EOL;
-		}
-		else{
-			echo successString('All well') . PHP_EOL;
+			echo noticeString('define changed') . PHP_EOL;
 		}
 		return true;
 	}
 	else{
-		echo errorString('makro not found!') . PHP_EOL;
+		echo errorString('define not found!') . PHP_EOL;
 	}
 	return false;
 }
@@ -109,6 +127,45 @@ function isDryRun(){
 		return false;
 	}
 	return true;
+}
+
+function getFileHeader($author, $description){
+	$out = <<<EOT
+/**
+ * Mandelbulber v2, a 3D fractal generator       ,=#MKNmMMKmmßMNWy,
+ *                                             ,B" ]L,,p%%%,,,§;, "K
+ * Copyright (C) 2014 Krzysztof Marczak        §R-==%w["'~5]m%=L.=~5N
+ *                                        ,=mm=§M ]=4 yJKA"/-Nsaj  "Bw,==,,
+ * This file is part of Mandelbulber.    §R.r= jw",M  Km .mM  FW ",§=ß., ,TN
+ *                                     ,4R =%["w[N=7]J '"5=],""]]M,w,-; T=]M
+ * Mandelbulber is free software:     §R.ß~-Q/M=,=5"v"]=Qf,'§"M= =,M.§ Rz]M"Kw
+ * you can redistribute it and/or     §w "xDY.J ' -"m=====WeC=\ ""%""y=%"]"" §
+ * modify it under the terms of the    "§M=M =D=4"N #"%==A%p M§ M6  R' #"=~.4M
+ * GNU General Public License as        §W =, ][T"]C  §  § '§ e===~ U  !§[Z ]N
+ * published by the                    4M",,Jm=,"=e~  §  §  j]]""N  BmM"py=ßM
+ * Free Software Foundation,          ]§ T,M=& 'YmMMpM9MMM%=w=,,=MT]M m§;'§,
+ * either version 3 of the License,    TWw [.j"5=~N[=§%=%W,T ]R,"=="Y[LFT ]N
+ * or (at your option)                   TW=,-#"%=;[  =Q:["V""  ],,M.m == ]N
+ * any later version.                      J§"mr"] ,=,," =="""J]= M"M"]==ß"
+ *                                          §= "=C=4 §"eM "=B:m\\4"]#F,§~
+ * Mandelbulber is distributed in            "9w=,,]w em%wJ '"~" ,=,,ß"
+ * the hope that it will be useful,                 . "K=  ,=RMMMßM"""
+ * but WITHOUT ANY WARRANTY;                            .'''
+ * without even the implied warranty
+ * of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ *
+ * See the GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License
+ * along with Mandelbulber. If not, see <http://www.gnu.org/licenses/>.
+ *
+ * ###########################################################################
+ *
+ * Authors: __author__
+ *
+ * __description__
+ */
+EOT;
+	return str_replace(array('__author__', '__description__'), array($author, $description), $out);
 }
 
 ?>
