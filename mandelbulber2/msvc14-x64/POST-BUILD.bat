c1a469518043dace660e7334e78ee1fb0481565e
mancoast <RobertPancoast77@gmail.com>
2016-12-29 12:45:19 -0500

    Simplify Visual Studio Build
    10377

--- mandelbulber2/msvc14-x64/POST-BUILD.bat
+++ mandelbulber2/msvc14-x64/POST-BUILD.bat
@@ -1,52 +1,53 @@
 REM @echo off
 
-Echo Mandelbulber2 Windoze Build Bot
+Echo Mandelbulber2 Visual Studio Post-Build
 
-REM # XEON x64 Build Vars #
+REM # x64 Build Vars #
 set _SCRIPT_DRIVE=%~d0
 set _SCRIPT_FOLDER=%~dp0
 set SRC=%CD%\..\..\
-set BUILDTREE=%SRC%\build-win
+set BUILDTREE=%SRC%build-win
 SET tbs_arch=x64
 SET vcvar_arg=x86_amd64
 SET qt_version=Qt5.7.0\5.7\msvc2015_64
 set PACKAGESDIR=%CD%\packages
-
-REM # build_script #
-set QTDIR=%PACKAGESDIR%\bulbqt-msvc14-x64.5.7.0.11077\build\native
-set GSLDIR=%PACKAGESDIR%\gsl-msvc14-x64.2.2.1.2677
-set PNGDIR=%PACKAGESDIR%\libpng.1.6.26.1
-set ZLIBDIR=%PACKAGESDIR%\zlib.1.2.8.8
-set TIFFDIR=%PACKAGESDIR%\libtiff.4.0.6.2
-set JPEGDIR=%PACKAGESDIR%\libjpeg.9.2.0.1
+set QTDIR=%PACKAGESDIR%\bulbqt-msvc14-x64.5.7.0.10377\build\native
 
 :copy_files
 set BINDIR=%BUILDTREE%\Release\
 echo %BINDIR%	
-xcopy /B /S %SRC%\mandelbulber2\deploy\share\mandelbulber2 %BINDIR%
-xcopy /I %QTDIR%\plugins\gamepads %BINDIR%gamepads
-xcopy /I %QTDIR%\plugins\iconengines %BINDIR%iconengines
-xcopy /I %QTDIR%\plugins\imageformats %BINDIR%imageformats
-xcopy /I %QTDIR%\plugins\platforms %BINDIR%platforms
-xcopy %QTDIR%\bin\Qt5Gamepad*.dll %BINDIR%
-xcopy %QTDIR%\bin\Qt5Gui*.dll %BINDIR%
-xcopy %QTDIR%\bin\Qt5Network*.dll %BINDIR%
-xcopy %QTDIR%\bin\Qt5Svg*.dll %BINDIR%
-xcopy %QTDIR%\bin\Qt5Test*.dll %BINDIR%
-xcopy %QTDIR%\bin\Qt5Widgets*.dll %BINDIR%
-xcopy %QTDIR%\bin\Qt5Core*.dll %BINDIR%
-xcopy %QTDIR%\bin\Qt5Multimedia*.dll %BINDIR%
-xcopy deps\zlib*.dll %BINDIR%
-xcopy deps\libpng*.dll %BINDIR%
-xcopy deps\tiff*.dll %BINDIR%
-del %BINDIR%language
-del %BINDIR%qt_data
-del %BINDIR%data
-xcopy /I %SRC%\mandelbulber2\language %BINDIR%language
-xcopy /I %SRC%\mandelbulber2\qt_data %BINDIR%qt_data
-xcopy /I %SRC%\mandelbulber2\data %BINDIR%data
-
-GOTO:eof
+xcopy /Y /I /S %SRC%\mandelbulber2\deploy\share\mandelbulber2 %BINDIR%
+xcopy /Y %QTDIR%\bin\Qt5Core.dll %BINDIR%
+xcopy /Y %QTDIR%\bin\Qt5Gamepad.dll %BINDIR%
+xcopy /Y %QTDIR%\bin\Qt5Gui.dll %BINDIR%
+xcopy /Y %QTDIR%\bin\Qt5Multimedia.dll %BINDIR%
+xcopy /Y %QTDIR%\bin\Qt5MultimediaQuick_p.dll %BINDIR%
+xcopy /Y %QTDIR%\bin\Qt5MultimediaWidgets.dll %BINDIR%
+xcopy /Y %QTDIR%\bin\Qt5Network.dll %BINDIR%
+xcopy /Y %QTDIR%\bin\Qt5Svg.dll %BINDIR%
+xcopy /Y %QTDIR%\bin\Qt5Test.dll %BINDIR%
+xcopy /Y %QTDIR%\bin\Qt5Widgets.dll %BINDIR%
+xcopy /Y /I %QTDIR%\plugins\gamepads %BINDIR%gamepads
+xcopy /Y /I %QTDIR%\plugins\iconengines %BINDIR%iconengines
+xcopy /Y /I %QTDIR%\plugins\imageformats %BINDIR%imageformats
+xcopy /Y /I %QTDIR%\plugins\platforms %BINDIR%platforms
+xcopy /Y /I %QTDIR%\plugins\audio %BINDIR%audio
+xcopy /Y /I %QTDIR%\plugins\mediaservice %BINDIR%mediaservice
+del /F /Q %BINDIR%language
+del /F /Q %BINDIR%qt_data
+del /F /Q %BINDIR%data
+xcopy /Y /I %SRC%\mandelbulber2\language %BINDIR%language
+xcopy /Y /I %SRC%\mandelbulber2\qt_data %BINDIR%qt_data
+xcopy /Y /I %SRC%\mandelbulber2\data %BINDIR%data
+copy /Y %SRC%\mandelbulber2\deploy\README-win32.txt %BINDIR%README.txt
+copy /Y %SRC%\mandelbulber2\deploy\NEWS %BINDIR%NEWS.txt
+copy /Y %SRC%\mandelbulber2\deploy\COPYING %BINDIR%COPYING.txt
+copy /Y %SRC%\mandelbulber2\deploy\win32\mandelbulber2.ico %BINDIR%mandelbulber2.ico
+mkdir %BINDIR%source
+xcopy /Y /I  %SRC%\mandelbulber2\src %BINDIR%source\src
+xcopy /Y /I  %SRC%\mandelbulber2\qt %BINDIR%source\qt
+mkdir %BINDIR%makefiles
+copy /Y %SRC%\mandelbulber2\Release\mandelbulber.pro %BINDIR%makefiles\mandelbulber.pro
 
 REM --- exit ----
 GOTO:eof
