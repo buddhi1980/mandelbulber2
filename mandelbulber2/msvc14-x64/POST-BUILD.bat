157784fabf36b0bc368d31d73a5dd045e0d10a5f
mancoast <RobertPancoast77@gmail.com>
2016-12-28 20:31:58 -0500

    Initialize VSO Post Build Batch
    PACKAGESDIR

--- mandelbulber2/msvc14-x64/POST-BUILD.bat
+++ mandelbulber2/msvc14-x64/POST-BUILD.bat
@@ -5,21 +5,20 @@ Echo Mandelbulber2 Windoze Build Bot
 REM # XEON x64 Build Vars #
 set _SCRIPT_DRIVE=%~d0
 set _SCRIPT_FOLDER=%~dp0
-set SRC=%CD%
+set SRC=%CD%\..\..\
 set BUILDTREE=%SRC%\build-win
 SET tbs_arch=x64
 SET vcvar_arg=x86_amd64
 SET qt_version=Qt5.7.0\5.7\msvc2015_64
-set PACKAGESDIR=%SRC%\mandelbulber2\msvc14-x64\packages
+set PACKAGESDIR=%CD%\packages
 
 REM # build_script #
-set QTDIR=%PACKAGESDIR%\bulbqt-msvc14-x64.5.7.0.11077
+set QTDIR=%PACKAGESDIR%\bulbqt-msvc14-x64.5.7.0.11077\build\native
 set GSLDIR=%PACKAGESDIR%\gsl-msvc14-x64.2.2.1.2677
 set PNGDIR=%PACKAGESDIR%\libpng.1.6.26.1
 set ZLIBDIR=%PACKAGESDIR%\zlib.1.2.8.8
 set TIFFDIR=%PACKAGESDIR%\libtiff.4.0.6.2
 set JPEGDIR=%PACKAGESDIR%\libjpeg.9.2.0.1
-cd %BUILDTREE%
 
 :copy_files
 set BINDIR=%BUILDTREE%\Release\
