bdc3d3767e370bdf4b3df5b9530c4e1125a94d1b
mancoast <RobertPancoast77@gmail.com>
2016-12-30 11:35:57 -0500

    Move Visual Studio Solution to Root
    cleanup ItemGroup & PACKAGESDIR
    required to enable Continuous Integration with multiple architectures
    
    Relocation of NuGet Config for VSO Build Agents
    
    Appveyor Packages

--- mandelbulber2/msvc14-x64/POST-BUILD.bat
+++ mandelbulber2/msvc14-x64/POST-BUILD.bat
@@ -10,7 +10,7 @@ set BUILDTREE=%SRC%build-win
 SET tbs_arch=x64
 SET vcvar_arg=x86_amd64
 SET qt_version=Qt5.7.0\5.7\msvc2015_64
-set PACKAGESDIR=%CD%\packages
+set PACKAGESDIR=%SRC%\packages
 set QTDIR=%PACKAGESDIR%\bulbqt-msvc14-x64.5.7.0.10377\build\native
 
 :copy_files
