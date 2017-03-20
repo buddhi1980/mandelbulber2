f45a6f6507fa4b80904651ac92b9cfa3bedc4efc
Robert Mancoast <RobertPancoast77@gmail.com>
2017-03-20 10:47:09 -0400

    rework icc

--- mandelbulber2/tools/DOE/build.batch.bat
+++ mandelbulber2/tools/DOE/build.batch.bat
@@ -14,6 +14,7 @@ SET vcvar_arg=x86_amd64
 SET ms_build_suffix=Bin\amd64
 SET cmake_platform="NMake Makefiles JOM"
 SET cmake_platform_tool="Intel C++ Compiler 17.0"
+SET VERBOSE_MAKEFILE=FALSE
 set QTDIR=%SRC%\packages\bulbqt-msvc14-x64.5.8.0.10782\build\native
 SET JomDir=%SRC%\packages\jom-nug.1.1.2.777\build\native
 set PATH=%QTDIR%\bin;C:\Program Files (x86)\MSBuild\14.0\%ms_build_suffix%;%SRC%;%PATH%
@@ -41,13 +42,16 @@ REM # packages from nuget #
 nuget restore %SRC%\mandelbulber2.sln
 
 REM # build_script #
-set ZLIBDIR=%SRC%\packages\zlib-msvc14-x64.1.2.11.7795
-set PNGDIR=%SRC%\packages\libpng.1.6.28.1
-set TIFFDIR=%SRC%\packages\libtiff.4.0.6.2
-set JPEGDIR=%SRC%\packages\libjpeg.9.2.0.1
-set GSLDIR=%SRC%\packages\gsl-msvc14-x64.2.3.0.2779
+set ZLIBDIR=%SRC%\packages\zlib-msvc14-x64.1.2.11.7795\build\native
+set ZLIBDIST=%ZLIBDIR%\bin_release
+set PNGDIR=%SRC%\packages\libpng.1.6.28.1\build\native
 set PNGDIST=%SRC%\packages\libpng.redist.1.6.28.1\build\native\bin\x64\v140\dynamic\Release
-set TIFFDIST=%SRC%\packages\libtiff.redist.4.0.6.2\build\native\bin\Release\x64\v140"
+set TIFFDIR=%SRC%\packages\libtiff.4.0.6.2\build\native
+set TIFFDIST=%SRC%\packages\libtiff.redist.4.0.6.2\build\native\bin\Release\x64\v140
+set JPEGDIR=%SRC%\packages\libjpeg.9.2.0.1\build\native
+set JPEGDIST=%SRC%\packages\libjpeg.redist.9.2.0.1\build\native\bin\v140\x64\Release\dynamic\cdecl
+set GSLDIR=%SRC%\packages\gsl-msvc14-x64.2.3.0.2779\build\native
+
 cd %BUILDTREE%
 cmake -G %cmake_platform% ^
 -DCMAKE_C_COMPILER=icl.exe ^
@@ -56,50 +60,52 @@ cmake -G %cmake_platform% ^
 -DCMAKE_CXX_LINK_EXECUTABLE=icl.exe ^
 -DCMAKE_CXX_STANDARD=11 ^
 -DCMAKE_MAKE_PROGRAM=%JomDir%\jom.exe ^
--DCMAKE_VERBOSE_MAKEFILE=TRUE ^
+-DCMAKE_VERBOSE_MAKEFILE=%VERBOSE_MAKEFILE% ^
 -DUSE_GAMEPAD=1 ^
--DCMAKE_CXX_FLAGS="/Qstd=c++11 /Qopenmp /EHsc" ^
+-DCMAKE_CXX_FLAGS="/Qstd=c++11 /Qopenmp /EHsc /nologo" ^
 -DCMAKE_BUILD_TYPE=Release ^
 -DCMAKE_INSTALL_PREFIX=%OutDir% ^
--DZLIB_LIBRARY=%ZLIBDIR%\build\native\lib_release\zlibstatic.lib ^
--DZLIB_INCLUDE_DIR=%ZLIBDIR%\build\native\include ^
--DPNG_LIBRARY=%PNGDIR%\build\native\lib\x64\v140\dynamic\Release\libpng16.lib ^
--DPNG_PNG_INCLUDE_DIR=%PNGDIR%\build\native\include ^
--DTIFF_LIBRARY=%TIFFDIR%\build\native\lib\Release\x64\v140\libtiff.lib ^
--DTIFF_INCLUDE_DIR=%TIFFDIR%\build\native\include ^
--DJPEG_LIBRARY=%JPEGDIR%\build\native\lib\v140\x64\Release\static\cdecl\jpeg.lib ^
--DJPEG_INCLUDE_DIR=%JPEGDIR%\build\native\include ^
--DGSL_CBLAS_LIBRARY=%GSLDIR%\build\native\static\gslcblas.lib ^
--DGSL_LIBRARY=%GSLDIR%\build\native\static\gsl.lib ^
--DGSL_INCLUDE_DIR=%GSLDIR%\build\native\ ^
+-DZLIB_LIBRARY=%ZLIBDIR%\lib_release\zlibstatic.lib ^
+-DZLIB_INCLUDE_DIR=%ZLIBDIR%\include ^
+-DPNG_LIBRARY=%PNGDIR%\lib\x64\v140\dynamic\Release\libpng16.lib ^
+-DPNG_PNG_INCLUDE_DIR=%PNGDIR%\include ^
+-DTIFF_LIBRARY=%TIFFDIR%\lib\Release\x64\v140\libtiff.lib ^
+-DTIFF_INCLUDE_DIR=%TIFFDIR%\include ^
+-DJPEG_LIBRARY=%JPEGDIR%\lib\v140\x64\Release\static\cdecl\jpeg.lib ^
+-DJPEG_INCLUDE_DIR=%JPEGDIR%\include ^
+-DGSL_CBLAS_LIBRARY=%GSLDIR%\static\gslcblas.lib ^
+-DGSL_LIBRARY=%GSLDIR%\static\gsl.lib ^
+-DGSL_INCLUDE_DIR=%GSLDIR%\ ^
 %SRC%\mandelbulber2\cmake\
 
-%JomDir%\jom.exe -j12
+cmake --build . --config Release --target install
 
 :copy_files
 set BINDIR=%OutDir%
 echo %BINDIR%	
-xcopy /B /S %SRC%\mandelbulber2\deploy\share\mandelbulber2 %BINDIR%
-xcopy /I %QTDIR%\plugins\gamepads %BINDIR%gamepads
-xcopy /I %QTDIR%\plugins\iconengines %BINDIR%iconengines
-xcopy /I %QTDIR%\plugins\imageformats %BINDIR%imageformats
-xcopy /I %QTDIR%\plugins\platforms %BINDIR%platforms
-xcopy %QTDIR%\bin\Qt5Gamepad*.dll %BINDIR%
-xcopy %QTDIR%\bin\Qt5Gui*.dll %BINDIR%
-xcopy %QTDIR%\bin\Qt5Network*.dll %BINDIR%
-xcopy %QTDIR%\bin\Qt5Svg*.dll %BINDIR%
-xcopy %QTDIR%\bin\Qt5Test*.dll %BINDIR%
-xcopy %QTDIR%\bin\Qt5Widgets*.dll %BINDIR%
-xcopy %QTDIR%\bin\Qt5Core*.dll %BINDIR%
-xcopy %QTDIR%\bin\Qt5Multimedia*.dll %BINDIR%
-xcopy %PNGDIST%\*.dll %BINDIR%
-xcopy %TIFFDIST%\*.dll %BINDIR%
+xcopy /Q /C /Y /B /S %SRC%\mandelbulber2\deploy\share\mandelbulber2 %BINDIR%
+xcopy /Q /C /Y /I %QTDIR%\plugins\gamepads %BINDIR%gamepads
+xcopy /Q /C /Y /I %QTDIR%\plugins\iconengines %BINDIR%iconengines
+xcopy /Q /C /Y /I %QTDIR%\plugins\imageformats %BINDIR%imageformats
+xcopy /Q /C /Y /I %QTDIR%\plugins\platforms %BINDIR%platforms
+xcopy /Q /C /Y %QTDIR%\bin\Qt5Gamepad*.dll %BINDIR%
+xcopy /Q /C /Y %QTDIR%\bin\Qt5Gui*.dll %BINDIR%
+xcopy /Q /C /Y %QTDIR%\bin\Qt5Network*.dll %BINDIR%
+xcopy /Q /C /Y %QTDIR%\bin\Qt5Svg*.dll %BINDIR%
+xcopy /Q /C /Y %QTDIR%\bin\Qt5Test*.dll %BINDIR%
+xcopy /Q /C /Y %QTDIR%\bin\Qt5Widgets*.dll %BINDIR%
+xcopy /Q /C /Y %QTDIR%\bin\Qt5Core*.dll %BINDIR%
+xcopy /Q /C /Y %QTDIR%\bin\Qt5Multimedia*.dll %BINDIR%
 del /F /Q %BINDIR%language
 del /F /Q %BINDIR%qt_data
 del /F /Q %BINDIR%data
-xcopy /I %SRC%\mandelbulber2\language %BINDIR%language
-xcopy /I %SRC%\mandelbulber2\qt_data %BINDIR%qt_data
-xcopy /I %SRC%\mandelbulber2\data %BINDIR%data
+xcopy /Q /C /Y /I %SRC%\mandelbulber2\language %BINDIR%language
+xcopy /Q /C /Y /I %SRC%\mandelbulber2\qt_data %BINDIR%qt_data
+xcopy /Q /C /Y /I %SRC%\mandelbulber2\data %BINDIR%data
+xcopy /Q /C /Y %ZLIBDIST%\*.dll %BINDIR%
+xcopy /Q /C /Y %PNGDIST%\*.dll %BINDIR%
+xcopy /Q /C /Y %TIFFDIST%\*.dll %BINDIR%
+xcopy /Q /C /Y %JPEGDIST%\*.dll %BINDIR%
 
 REM --- exit ----
 GOTO:eof
