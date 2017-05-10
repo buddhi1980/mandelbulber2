cebb75f14f871930d5be2243a9de73261b60c9c7
buddhi1980 <buddhi1980@gmail.com>
2017-05-10 20:47:38 +0200

    added missing opencl_typedefs.h

---
+++ mandelbulber2/opencl/opencl_typedefs.h
@@ -0,0 +1,18 @@
+/*
+ * opencl_typedefs.h
+ *
+ *  Created on: 10 maj 2017
+ *      Author: krzysztof
+ */
+
+#ifndef MANDELBULBER2_OPENCL_OPENCL_TYPEDEFS_H_
+#define MANDELBULBER2_OPENCL_OPENCL_TYPEDEFS_H_
+
+typedef float3 cl_float3;
+typedef float cl_float;
+typedef int cl_int;
+typedef unsigned int cl_uint;
+typedef unsigned short cl_ushort;
+typedef unsigned char cl_uchar;
+
+#endif /* MANDELBULBER2_OPENCL_OPENCL_TYPEDEFS_H_ */
