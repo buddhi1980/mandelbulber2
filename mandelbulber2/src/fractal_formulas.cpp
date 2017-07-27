0c2e63619c70fe84049695c208a56b9919df3327
GraemeMcLaren <mclarekin@gmail.com>
2017-07-27 20:44:35 +1200

    finished coding mandelbulb juliabulb

--- mandelbulber2/src/fractal_formulas.cpp
+++ mandelbulber2/src/fractal_formulas.cpp
@@ -2678,44 +2678,51 @@ void MandelbulbBermarteIteration(CVector4 &z, const sFractal *fractal, sExtended
  */
 void MandelbulbJuliabulbIteration(CVector4 &z, const sFractal *fractal, sExtendedAux &aux)
 {
+	// mandelbulb multi
 	if (aux.i >= fractal->transformCommon.startIterationsM
 			&& aux.i < fractal->transformCommon.stopIterationsM)
 	{
+		aux.r = z.Length();
 		if (fractal->transformCommon.functionEnabledFalse)
 		{
-			if (fractal->transformCommon.functionEnabledAxFalse) z.x = fabs(z.x);
-			if (fractal->transformCommon.functionEnabledAyFalse) z.y = fabs(z.y);
-			if (fractal->transformCommon.functionEnabledAzFalse) z.z = fabs(z.z);
+			if (fractal->transformCommon.functionEnabledAxFalse
+					&& aux.i >= fractal->transformCommon.startIterationsX
+					&& aux.i < fractal->transformCommon.stopIterationsX)
+				z.x = fabs(z.x);
+			if (fractal->transformCommon.functionEnabledAyFalse
+					&& aux.i >= fractal->transformCommon.startIterationsY
+					&& aux.i < fractal->transformCommon.stopIterationsY)
+				z.y = fabs(z.y);
+			if (fractal->transformCommon.functionEnabledAzFalse
+					&& aux.i >= fractal->transformCommon.startIterationsZ
+					&& aux.i < fractal->transformCommon.stopIterationsZ)
+				z.z = fabs(z.z);
 		}
 
 		double th0 = fractal->bulb.betaAngleOffset;
 		double ph0 = fractal->bulb.alphaAngleOffset;
+
 		CVector3 v;
-
-		switch (fractal->mandelbulbMulti.orderOfXYZ)
+		switch (fractal->sinTan2Trig.orderOfZYX)
 		{
-			case multi_OrderOfXYZ_xyz:
-			default: v = CVector3(z.x, z.y, z.z); break;
-			case multi_OrderOfXYZ_xzy: v = CVector3(z.x, z.z, z.y); break;
-			case multi_OrderOfXYZ_yxz: v = CVector3(z.y, z.x, z.z); break;
-			case multi_OrderOfXYZ_yzx: v = CVector3(z.y, z.z, z.x); break;
-			case multi_OrderOfXYZ_zxy: v = CVector3(z.z, z.x, z.y); break;
-			case multi_OrderOfXYZ_zyx: v = CVector3(z.z, z.y, z.x); break;
+			case multi_OrderOfZYX_zyx:
+			default: v = CVector3(z.z, z.y, z.x); break;
+			case multi_OrderOfZYX_zxy: v = CVector3(z.z, z.x, z.y); break;
+			case multi_OrderOfZYX_yzx: v = CVector3(z.y, z.z, z.x); break;
+			case multi_OrderOfZYX_yxz: v = CVector3(z.y, z.x, z.z); break;
+			case multi_OrderOfZYX_xzy: v = CVector3(z.x, z.z, z.y); break;
+			case multi_OrderOfZYX_xyz: v = CVector3(z.x, z.y, z.z); break;
 		}
-		// if (aux.r < 1e-21)
-		//	aux.r = 1e-21;
-		// if (v3 < 1e-21 && v3 > -1e-21)
-		//	v3 = (v3 > 0) ? 1e-21 : -1e-21;
 
-		if (fractal->mandelbulbMulti.acosOrAsin == multi_acosOrAsin_acos)
-			th0 += acos(v.x / aux.r);
-		else
+		if (fractal->sinTan2Trig.asinOrAcos == multi_asinOrAcos_asin)
 			th0 += asin(v.x / aux.r);
-
-		if (fractal->mandelbulbMulti.atanOrAtan2 == multi_atanOrAtan2_atan)
-			ph0 += atan(v.y / v.z);
 		else
+			th0 += acos(v.x / aux.r);
+
+		if (fractal->sinTan2Trig.atan2OrAtan == multi_atan2OrAtan_atan2)
 			ph0 += atan2(v.y, v.z);
+		else
+			ph0 += atan(v.y / v.z);
 
 		double rp = pow(aux.r, fractal->bulb.power - 1.0);
 		double th = th0 * fractal->bulb.power * fractal->transformCommon.scaleA1;
@@ -2732,15 +2739,20 @@ void MandelbulbJuliabulbIteration(CVector4 &z, const sFractal *fractal, sExtende
 			z = rp * CVector4(sinth * sin(ph), cos(ph) * sinth, cos(th), 0.0);
 		}
 		else
-		{ // sine mode ( default = V2.07))
+		{ // sine mode
 			double costh = th;
 			if (fractal->transformCommon.functionEnabledzFalse) costh = th0;
 			costh = cos(costh);
 			z = rp * CVector4(costh * cos(ph), sin(ph) * costh, sin(th), 0.0);
 		}
 	}
+
+
+
+
 	// sym4
-	if (aux.i >= fractal->transformCommon.startIterationsD
+	if (fractal->transformCommon.functionEnabled
+			&& aux.i >= fractal->transformCommon.startIterationsD
 			&& aux.i < fractal->transformCommon.stopIterationsD)
 	{
 		aux.r = z.Length();
@@ -2774,6 +2786,31 @@ void MandelbulbJuliabulbIteration(CVector4 &z, const sFractal *fractal, sExtende
 		z *= fractal->transformCommon.scale1;
 		aux.r_dz *= fabs(fractal->transformCommon.scale1);*/
 	}
+
+	// sym3 msltoe eiffie
+	if (fractal->transformCommon.functionEnabledEFalse
+			&& aux.i >= fractal->transformCommon.startIterationsE
+			&& aux.i < fractal->transformCommon.stopIterationsE)
+	{
+		aux.r = z.Length();
+		double psi = fabs(fmod(atan2(z.z, z.y) + M_PI + M_PI_8, M_PI_4) - M_PI_8);
+		double lengthYZ = sqrt(z.y * z.y + z.z * z.z);
+
+		z.y = cos(psi) * lengthYZ;
+		z.z = sin(psi) * lengthYZ;
+		aux.r_dz = aux.r_dz * 2.0 * aux.r;
+
+		CVector4 z2 = z * z;
+		double rr = z2.x + z2.y + z2.z;
+		double m = 1.0 - z2.z / rr;
+		CVector4 temp;
+		temp.x = (z2.x - z2.y) * m;
+		temp.y = 2.0 * z.x * z.y * m * fractal->transformCommon.scale; // scaling y;;
+		temp.z = 2.0 * z.z * sqrt(z2.x + z2.y);
+		temp.w = z.w;
+		z = temp + fractal->transformCommon.additionConstant000;
+	}
+
 	if (fractal->transformCommon.addCpixelEnabledFalse)
 	{
 		CVector4 c = aux.const_c;
