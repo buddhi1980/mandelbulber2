12199c4ee6dd11a1f011ecb63101054035f5d0c7
Sebastian Jennen <sebastian.jennen@gmx.de>
2017-08-25 07:54:57 +0200

    fixed whitespace in opencl include path problem
    see #351
    
    This should be all, the compiler argument was not properly escaped, now
    with quotes it should work.
    
    @buddhi1980 can you check with windows?

--- mandelbulber2/src/opencl_engine.cpp
+++ mandelbulber2/src/opencl_engine.cpp
@@ -115,7 +115,7 @@ bool cOpenClEngine::Build(const QByteArray &programString, QString *errorText)
 			{
 				QString openclPath = systemData.sharedDir + "opencl" + QDir::separator();
 				std::string buildParams = "-w -cl-single-precision-constant -cl-denorms-are-zero";
-				buildParams.append(" -DOPENCL_KERNEL_CODE -I" + openclPath.toStdString());
+				buildParams.append(" -DOPENCL_KERNEL_CODE -I\"" + openclPath.toStdString() + "\"");
 				buildParams += definesCollector.toUtf8().constData();
 				qDebug() << "Build parameters: " << buildParams.c_str();
 				err = program->build(hardware->getClDevices(), buildParams.c_str());
