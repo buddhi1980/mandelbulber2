aa95add4d222535f39e44f174a067ed7b7c1a58e
mancoast <RobertPancoast77@gmail.com>
2017-09-17 12:38:06 -0400

    move cast to separate header

---
+++ mandelbulber2/src/cast.hpp
@@ -0,0 +1,50 @@
+/**
+ * Mandelbulber v2, a 3D fractal generator       ,=#MKNmMMKmmßMNWy,
+ *                                             ,B" ]L,,p%%%,,,§;, "K
+ * Copyright (C) 2014-17 Mandelbulber Team     §R-==%w["'~5]m%=L.=~5N
+ *                                        ,=mm=§M ]=4 yJKA"/-Nsaj  "Bw,==,,
+ * This file is part of Mandelbulber.    §R.r= jw",M  Km .mM  FW ",§=ß., ,TN
+ *                                     ,4R =%["w[N=7]J '"5=],""]]M,w,-; T=]M
+ * Mandelbulber is free software:     §R.ß~-Q/M=,=5"v"]=Qf,'§"M= =,M.§ Rz]M"Kw
+ * you can redistribute it and/or     §w "xDY.J ' -"m=====WeC=\ ""%""y=%"]"" §
+ * modify it under the terms of the    "§M=M =D=4"N #"%==A%p M§ M6  R' #"=~.4M
+ * GNU General Public License as        §W =, ][T"]C  §  § '§ e===~ U  !§[Z ]N
+ * published by the                    4M",,Jm=,"=e~  §  §  j]]""N  BmM"py=ßM
+ * Free Software Foundation,          ]§ T,M=& 'YmMMpM9MMM%=w=,,=MT]M m§;'§,
+ * either version 3 of the License,    TWw [.j"5=~N[=§%=%W,T ]R,"=="Y[LFT ]N
+ * or (at your option)                   TW=,-#"%=;[  =Q:["V""  ],,M.m == ]N
+ * any later version.                      J§"mr"] ,=,," =="""J]= M"M"]==ß"
+ *                                          §= "=C=4 §"eM "=B:m|4"]#F,§~
+ * Mandelbulber is distributed in            "9w=,,]w em%wJ '"~" ,=,,ß"
+ * the hope that it will be useful,                 . "K=  ,=RMMMßM"""
+ * but WITHOUT ANY WARRANTY;                            .'''
+ * without even the implied warranty
+ * of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ *
+ * See the GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License
+ * along with Mandelbulber. If not, see <http://www.gnu.org/licenses/>.
+ *
+ * ###########################################################################
+ *
+ * Authors: Robert Pancoast (RobertPancoast77@gmail.com)
+ *
+ * Functions to Help safely cast objects within limits of their types.
+ */
+
+#ifndef MANDELBULBER2_SRC_CAST_HPP_
+#define MANDELBULBER2_SRC_CAST_HPP_
+
+#include <limits.h>
+
+// Safe Cast Helper for size_t ==> int
+inline int CastSizeToInt(size_t sizeValue) {
+	return (sizeValue <= std::numeric_limits<int>::max()) ? int(size_t(sizeValue)) : -1;
+}
+
+// Safe Cast Helper for int ==> size_t
+inline size_t CastIntToSize(int intValue) {
+	return (intValue < 0) ? std::numeric_limits<size_t>::max() : size_t(unsigned(intValue));
+}
+
+#endif /* MANDELBULBER2_SRC_CAST_HPP_ */
