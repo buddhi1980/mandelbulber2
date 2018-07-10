84a5f5496ea44488cb0fa46e7ee9d2c5cef5aff0
mclarekin <mclarekin@gmail.com>
2018-07-10 20:02:39 +1200

    Finished update of transf_platonic_solid

--- mandelbulber2/src/fractal_formulas.cpp
+++ mandelbulber2/src/fractal_formulas.cpp
@@ -9826,57 +9826,56 @@ void TransfParabFoldIteration(CVector4 &z, const sFractal *fractal, sExtendedAux
 }
 
 /**
- * platonic solid
+ * platonic solid - from Darkbeams code
  * @reference
  * http://www.fractalforums.com/3d-fractal-generation/platonic-dimensions/msg36528/#msg36528
  */
 void TransfPlatonicSolidIteration(CVector4 &z, const sFractal *fractal, sExtendedAux &aux)
-/*{
-	Q_UNUSED(aux);
-
-	double rho = sqrt(z.Length()); // the radius
-	double theta =
-		cos(fractal->platonicSolid.frequency * z.x) * sin(fractal->platonicSolid.frequency * z.y)
-		+ cos(fractal->platonicSolid.frequency * z.y) * sin(fractal->platonicSolid.frequency * z.z)
-		+ cos(fractal->platonicSolid.frequency * z.z) * sin(fractal->platonicSolid.frequency * z.x);
-	double r = theta * fractal->platonicSolid.amplitude + rho * fractal->platonicSolid.rhoMul;
-	z *= r;
-}*/
 {
-	//Q_UNUSED(aux);
-
+	double platonicR = 0.0;
 	double rho = 0.0;
-
-	/*if (fractal->transformCommon.functionEnabledx) rho = sqrt(z.Length()); // the radius
-	if (fractal->transformCommon.functionEnabledyFalse) rho = z.Length();
-	if (fractal->transformCommon.functionEnabledzFalse) rho = z.Dot(z);
-	*/
-
-	double rho1 = 0.0;
-	double rho2 = 0.0;
-	double rho3 = 0.0;
-	if (fractal->transformCommon.functionEnabledx) rho1 = sqrt(z.Length()); // the radius
-	if (fractal->transformCommon.functionEnabledyFalse) rho2 = z.Length();
-	if (fractal->transformCommon.functionEnabledzFalse) rho3 = z.Dot(z);
-
-	if (fractal->transformCommon.functionEnabledAxFalse)
-		rho = rho1 + (rho2 - rho1) * fractal->transformCommon.scale1;
-	else if (fractal->transformCommon.functionEnabledAyFalse)
-		rho = rho2 + (rho3 - rho2) * fractal->transformCommon.scale1;
+	if (!fractal->transformCommon.functionEnabledFalse)
+	{
+		rho = sqrt(aux.r); // the radius
+		double theta =
+			cos(fractal->platonicSolid.frequency * z.x) * sin(fractal->platonicSolid.frequency * z.y)
+			+ cos(fractal->platonicSolid.frequency * z.y) * sin(fractal->platonicSolid.frequency * z.z)
+			+ cos(fractal->platonicSolid.frequency * z.z) * sin(fractal->platonicSolid.frequency * z.x);
+		platonicR = theta * fractal->platonicSolid.amplitude + rho * fractal->platonicSolid.rhoMul;
+		z *= platonicR;
+	}
 	else
-		rho = rho1 + rho2 + rho3;
+	{
+		//if (fractal->transformCommon.functionEnabledx) rho = sqrt(z.Length()); // the radius
+		//if (fractal->transformCommon.functionEnabledyFalse) rho = z.Length();
+		//if (fractal->transformCommon.functionEnabledzFalse) rho = z.Dot(z);
 
-	double theta =
-		cos(fractal->platonicSolid.frequency * z.x) * sin(fractal->platonicSolid.frequency * z.y)
-		+ cos(fractal->platonicSolid.frequency * z.y) * sin(fractal->platonicSolid.frequency * z.z)
-		+ cos(fractal->platonicSolid.frequency * z.z) * sin(fractal->platonicSolid.frequency * z.x);
-		// theta is pos or neg && < 3.0
+		double rho1 = 0.0;
+		double rho2 = 0.0;
+		double rho3 = 0.0;
+		if (fractal->transformCommon.functionEnabledx) rho1 = sqrt(aux.r);
+		if (fractal->transformCommon.functionEnabledyFalse) rho2 = aux.r;
+		if (fractal->transformCommon.functionEnabledzFalse) rho3 = aux.r * aux.r;
 
-	if (fractal->transformCommon.functionEnabledFalse) theta = fabs(theta);
+		if (fractal->transformCommon.functionEnabledAxFalse)
+			rho = rho1 + (rho2 - rho1) * fractal->transformCommon.scale1;
+		else if (fractal->transformCommon.functionEnabledAyFalse)
+			rho = rho2 + (rho3 - rho2) * fractal->transformCommon.scale1;
+		else
+			rho = rho1 + rho2 + rho3;
 
-	double platonicR = theta * fractal->platonicSolid.amplitude + rho * fractal->platonicSolid.rhoMul;
+		double theta =
+			cos(fractal->platonicSolid.frequency * z.x) * sin(fractal->platonicSolid.frequency * z.y)
+			+ cos(fractal->platonicSolid.frequency * z.y) * sin(fractal->platonicSolid.frequency * z.z)
+			+ cos(fractal->platonicSolid.frequency * z.z) * sin(fractal->platonicSolid.frequency * z.x);
+			// theta is pos or neg && < 3.0
 
-	z *= platonicR; //  can be neg
+		if (fractal->transformCommon.functionEnabledAzFalse) theta = fabs(theta);
+
+		platonicR = theta * fractal->platonicSolid.amplitude + rho * fractal->platonicSolid.rhoMul;
+
+		z *= platonicR; //  can be neg
+	}
 
 	if (fractal->analyticDE.enabled)
 	{
