7eb29d0421b994b59679aa6ccbf904a61275eea3
mclarekin <mclarekin@gmail.com>
2018-12-02 23:31:58 +1300

    transf_sin_and_cos_max.ui Beta

--- mandelbulber2/src/fractal_formulas.cpp
+++ mandelbulber2/src/fractal_formulas.cpp
@@ -11587,41 +11587,37 @@ void TransfSinAndCosMaxIteration(CVector4 &z, const sFractal *fractal, sExtended
 
 	if (fractal->transformCommon.functionEnabledAx)
 	{
-		sinZ.x = sin(oldZ.x * fractal->transformCommon.constantMultiplierA111.x * M_PI_2)
+		sinZ.x = sin(oldZ.x * M_PI_2x / fractal->transformCommon.constantMultiplierA111.x)
 				* fractal->transformCommon.scaleA1; // freq
-		cosZ.x = cos(oldZ.x * fractal->transformCommon.constantMultiplierB111.x * M_PI_2)
+		cosZ.x = cos(oldZ.x * M_PI_2x / fractal->transformCommon.constantMultiplierB111.x)
 				* fractal->transformCommon.scaleB1;
 		sinCosZ.x = sinZ.x * cosZ.x * fractal->transformCommon.scaleC1;
 		maxZ.x = max(max(sinZ.x, cosZ.x), sinCosZ.x);
 
-
 		if (fractal->transformCommon.functionEnabledFalse)
 			maxZ.x /= (fabs(oldZ.x) + 1.0);
-		//sinCosZ.x *= fractal->transformCommon.constantMultiplierC111.x;
 	}
 
 	if (fractal->transformCommon.functionEnabledAyFalse)
 	{
-		sinZ.y = sin(oldZ.y * fractal->transformCommon.constantMultiplierA111.y * M_PI_2)
+		sinZ.y = sin(oldZ.y * M_PI_2x / fractal->transformCommon.constantMultiplierA111.y)
 				* fractal->transformCommon.scaleA1; // freq
-		cosZ.y = cos(oldZ.y * fractal->transformCommon.constantMultiplierB111.y * M_PI_2)
+		cosZ.y = cos(oldZ.y * M_PI_2x / fractal->transformCommon.constantMultiplierB111.y)
 				* fractal->transformCommon.scaleB1;
 		sinCosZ.y = sinZ.y * cosZ.y * fractal->transformCommon.scaleC1;
 		maxZ.y = max(max(sinZ.y, cosZ.y), sinCosZ.y);
 
 		if (fractal->transformCommon.functionEnabledFalse)
 			maxZ.y /= (fabs(oldZ.y) + 1.0);
-
 	}
 
 	if (fractal->transformCommon.functionEnabledAzFalse)
 	{
-		sinZ.z = sin(oldZ.z * fractal->transformCommon.constantMultiplierA111.z * M_PI_2)
+		sinZ.z = sin(oldZ.z * M_PI_2x / fractal->transformCommon.constantMultiplierA111.z)
 				* fractal->transformCommon.scaleA1; // freq
-		cosZ.z = cos(oldZ.z * fractal->transformCommon.constantMultiplierB111.z * M_PI_2)
+		cosZ.z = cos(oldZ.z * M_PI_2x / fractal->transformCommon.constantMultiplierB111.z)
 				* fractal->transformCommon.scaleB1;
 		sinCosZ.z = sinZ.z * cosZ.z * fractal->transformCommon.scaleC1;
-
 		maxZ.z = max(max(sinZ.z, cosZ.z), sinCosZ.z);
 
 		if (fractal->transformCommon.functionEnabledFalse)
@@ -11651,18 +11647,6 @@ void TransfSinAndCosMaxIteration(CVector4 &z, const sFractal *fractal, sExtended
 				maxZ *=fabs(oldZ); break;
 		}
 	}
-
-
-
-
-
-	// diminish
-	/*if (fractal->transformCommon.functionEnabledFalse)
-	{
-		maxZ.x = maxZ.x / (fabs(oldZ.x) + 1.0);
-		maxZ.y = maxZ.y / (fabs(oldZ.y) + 1.0);
-		maxZ.z = maxZ.z / (fabs(oldZ.z) + 1.0);
-	}*/
 
 	z = maxZ;
 
