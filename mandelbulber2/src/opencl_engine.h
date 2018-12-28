c4a779bdd37f634dbaaa26a4e831fe12f8a06f8f
buddhi1980 <buddhi1980@gmail.com>
2018-12-28 11:29:05 +0100

    Added cOpenClOutputQueue class (will be used for multiGPU rendering)

--- mandelbulber2/src/opencl_engine.h
+++ mandelbulber2/src/opencl_engine.h
@@ -41,6 +41,7 @@
 
 #include "error_message.hpp"
 #include "include_header_wrapper.hpp"
+#include "opencl_input_output_buffer.h"
 
 class cOpenClHardware;
 class cParameterContainer;
@@ -84,23 +85,6 @@ public:
 	~cOpenClEngine() override;
 
 #ifdef USE_OPENCL
-	struct sClInputOutputBuffer
-	{
-		sClInputOutputBuffer(qint64 itemSize, qint64 length, QString name)
-				: itemSize(itemSize), length(length), name(std::move(name))
-		{
-		}
-
-		static void Deleter(char *charArray) { delete[] charArray; }
-
-		qint64 size() const { return itemSize * length; }
-		qint64 itemSize;
-		qint64 length;
-		QString name;
-		QSharedPointer<char> ptr;
-		QSharedPointer<cl::Buffer> clPtr;
-	};
-
 	void Lock();
 	void Unlock();
 	static void DeleteKernelCache();
---
+++ mandelbulber2/src/opencl_worker_output_queue.h
@@ -0,0 +1,49 @@
+/*
+ * opencl_worker_output_queue.h
+ *
+ *  Created on: 28 gru 2018
+ *      Author: krzysztof
+ */
+
+#ifndef MANDELBULBER2_SRC_OPENCL_WORKER_OUTPUT_QUEUE_H_
+#define MANDELBULBER2_SRC_OPENCL_WORKER_OUTPUT_QUEUE_H_
+
+#include <QtCore>
+
+class cOpenCLWorkerOutputQueue
+{
+public:
+	struct sClDataBuffer
+	{
+		sClDataBuffer(qint64 itemSize, qint64 length) : itemSize(itemSize), length(length)
+		{
+			data.reserve(itemSize * length);
+		}
+
+		qint64 size() const { return itemSize * length; }
+		qint64 itemSize;
+		qint64 length;
+		std::vector<char> data;
+	};
+
+	struct sClSingleOutput
+	{
+		qint64 jobX;
+		qint64 jobY;
+		qint64 jobWidth;
+		qint64 jobHeight;
+		QList<sClDataBuffer> outputBuffers;
+	};
+
+	void AddToQueue(const sClSingleOutput *data);
+	sClSingleOutput GetFromQueue();
+	bool isEmpty() { return queue.isEmpty(); }
+
+private:
+	QQueue<sClSingleOutput> queue;
+
+	cOpenCLWorkerOutputQueue();
+	~cOpenCLWorkerOutputQueue();
+};
+
+#endif /* MANDELBULBER2_SRC_OPENCL_WORKER_OUTPUT_QUEUE_H_ */
