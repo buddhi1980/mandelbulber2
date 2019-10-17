26d4459921375638f95d1b3e7342de9f0ae92bb9
Kovacsics Robert <rmk35@cam.ac.uk>
2019-10-17 13:44:12 +0100

    Fix patch-templates.sh script
    Turns out bash doesn't support null as field separator, so I have
    settled for newlines, hopefully no file names contain newlines.
    
    Also, taking $$ROOT/deploy and $$SHARED_PATH as arguments since they
    aren't in the environment

--- mandelbulber2/deploy/patch-templates.sh
+++ mandelbulber2/deploy/patch-templates.sh
@@ -1,6 +1,16 @@
 #!/bin/sh
 
-IFS='\0'
-for f in `find deploy -name '*.template' -print0`; do
-  envsubst '$SHARED_PATH' < $f > ${f%.template}
+# $1 is deploy directory, $2 is SHARED_PATH, or /usr if unset
+if [ -z "$1" ]; then
+  echo "Usage: $0 <path to mandelbulber2/deploy directory> [SHARED_PATH]"
+  exit 1
+fi
+
+export SHARED_PATH=${2:-/usr}
+
+# Names can contain spaces but hopefully they don't contain newlines
+IFS='
+'
+for f in `find "$1" -name '*.template'`; do
+  envsubst '$SHARED_PATH' < "$f" > "${f%.template}"
 done
--- mandelbulber2/qmake/installer.pri
+++ mandelbulber2/qmake/installer.pri
@@ -12,7 +12,7 @@ unix {
 
 	sharefiles.files = $$ROOT/deploy/share/mandelbulber2
 	sharefiles.path = $$PREFIX/share
-	sharefiles.extra = $$ROOT/deploy/patch-templates.sh
+	sharefiles.extra = $$ROOT/deploy/patch-templates.sh $$ROOT/deploy $$SHARED_PATH
 	INSTALLS += sharefiles
 	
 	formulaui.files = $$ROOT/formula/ui
