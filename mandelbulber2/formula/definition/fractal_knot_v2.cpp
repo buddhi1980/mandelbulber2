8d9c514cc8e282ba7cb1904bf9bfd3d281b64594
mclarekin <mclarekin@gmail.com>
2020-01-26 18:02:07 +1300

    knotv2beta

--- mandelbulber2/formula/definition/fractal_knot_v2.cpp
+++ mandelbulber2/formula/definition/fractal_knot_v2.cpp
@@ -28,49 +28,34 @@ cFractalKnotV2::cFractalKnotV2() : cAbstractFractal()
 
 void cFractalKnotV2::FormulaCode(CVector4 &z, const sFractal *fractal, sExtendedAux &aux)
 {
-	double polyfoldOrder = fractal->transformCommon.int2;
-
 	if (fractal->transformCommon.functionEnabledAxFalse) z.x = fabs(z.x);
 	if (fractal->transformCommon.functionEnabledAyFalse) z.y = fabs(z.y);
 	if (fractal->transformCommon.functionEnabledAzFalse) z.z = fabs(z.z);
 	z += fractal->transformCommon.offset000;
 
 	CVector4 zc = z;
-	zc.z *= fractal->transformCommon.scaleA1;
-	double mobius =
-		(1.0 * fractal->transformCommon.intA1 + fractal->transformCommon.intB1 / polyfoldOrder)
-		* atan2(zc.y, zc.x);
 
-	zc.x = sqrt(zc.x * zc.x + zc.y * zc.y) - fractal->transformCommon.offsetA2;
-	double temp = zc.x;
-	double c = cos(mobius);
-	double s = sin(mobius);
-	zc.x = c * zc.x + s * zc.z;
-	zc.z = -s * temp + c * zc.z;
+	double rxz = sqrt(zc.x * zc.x + zc.z * zc.z);
+	double ang = atan2(zc.z, zc.x);
+	double y = zc.y;
 
-	double m = 1.0 * polyfoldOrder / M_PI_2x;
-	double angle1 = floor(0.5 + m * (M_PI_2 - atan2(zc.x, zc.z))) / m;
+	for(int n = 0; n < fractal->transformCommon.int1; n++)
+	{
+	zc = CVector4(rxz, y, ang + M_PI_2x * n, 0.0);
 
-	temp = zc.x;
-	c = cos(angle1);
-	s = sin(angle1);
-	zc.x = c * zc.x + s * zc.z;
-	zc.z = -s * temp + c * zc.z;
+	zc.x -= fractal->transformCommon.offsetA2;
 
-	zc.x -= fractal->transformCommon.offset05;
+	double ra = zc.z * fractal->transformCommon.int2 / fractal->transformCommon.int3;
+	double raz = zc.z *fractal->transformCommon.int6 / fractal->transformCommon.int3;
 
-	double len = sqrt(zc.x * zc.x + zc.z * zc.z);
+	zc.x = zc.x - (fractal->transformCommon.offset1 * cos(ra) + fractal->transformCommon.offsetA2);
+	zc.y = zc.y - (fractal->transformCommon.offset1 * sin(raz) + fractal->transformCommon.offsetA2);
 
-	if (fractal->transformCommon.functionEnabledCFalse) len = min(len, max(fabs(zc.x), fabs(zc.z)));
+	double twist = sqrt(zc.x * zc.x +  zc.y * zc.y) - fractal->transformCommon.offset01;
+	if (fractal->transformCommon.functionEnabledCFalse) twist = min(twist, max(abs(zc.x), abs(zc.z)));
 
-	if (fractal->transformCommon.functionEnabledEFalse) z = zc;
-	double colorDist = aux.dist;
-	if (!fractal->transformCommon.functionEnabledDFalse)
-		aux.DE0 = len - fractal->transformCommon.offset01;
-	else
-		aux.DE0 = min(aux.dist, len - fractal->transformCommon.offset01);
-	aux.dist = aux.DE0 / aux.DE;
-
+	aux.dist = min(aux.dist, twist);
+	}
 	// aux.color
 	if (fractal->foldColor.auxColorEnabled)
 	{
@@ -80,12 +65,12 @@ void cFractalKnotV2::FormulaCode(CVector4 &z, const sFractal *fractal, sExtended
 		if (fmod(ang, 2.0) < 1.0) colorAdd += fractal->foldColor.difs0000.x;
 		colorAdd += fractal->foldColor.difs0000.y * fabs(ang);
 		colorAdd += fractal->foldColor.difs0000.z * fabs(ang * zc.x);
-		colorAdd += fractal->foldColor.difs0000.w * angle1;
+//		colorAdd += fractal->foldColor.difs0000.w * angle1;
 
 		colorAdd += fractal->foldColor.difs1;
 		if (fractal->foldColor.auxColorEnabledA)
 		{
-			if (colorDist != aux.dist) aux.color += colorAdd;
+//			if (colorDist != aux.dist) aux.color += colorAdd;
 		}
 		else
 			aux.color += colorAdd;
