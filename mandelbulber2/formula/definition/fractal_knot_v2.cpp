7c36131c44065a8fd21e141372e01ac1cc6374e1
mclarekin <mclarekin@gmail.com>
2020-01-28 21:39:24 +1300

    knotv2

--- mandelbulber2/formula/definition/fractal_knot_v2.cpp
+++ mandelbulber2/formula/definition/fractal_knot_v2.cpp
@@ -34,51 +34,46 @@ void cFractalKnotV2::FormulaCode(CVector4 &z, const sFractal *fractal, sExtended
 	z += fractal->transformCommon.offset000;
 
 	CVector4 zc = z;
-	zc.z *= fractal->transformCommon.scaleA1;
-	double rxz = sqrt(zc.x * zc.x + zc.z * zc.z);
-	double ang = atan2(zc.z, zc.x);
-	double y = zc.y;
-	double twist = 0.0;
+	double tempA = zc.y;
+	double tempB = zc.z;
+	if (fractal->transformCommon.functionEnabledJFalse) swap(tempA, tempB);
 
-	for(int n = 0; n < fractal->transformCommon.int1; n++)
+	zc.z *= fractal->transformCommon.scaleA1;
+	double rxz = sqrt(zc.x * zc.x + tempA * tempA);
+	double ang = atan2(tempA, zc.x);
+	double t = tempB;
+
+	double colorDist = 0.0;
+	for(int n = 0; n < fractal->transformCommon.int3; n++)
 	{
-		zc = CVector4(rxz, y, ang + M_PI_2x * n, 0.0);
+		zc = CVector4(rxz, t, ang + M_PI_2x * n, 0.0);
 
 		zc.x -= fractal->transformCommon.offsetA2;
 
-		double ra = zc.z * fractal->transformCommon.int2 / fractal->transformCommon.int3;
-		double raz = zc.z *fractal->transformCommon.int6 / fractal->transformCommon.int3;
+		double ra = zc.z * fractal->transformCommon.int3X / fractal->transformCommon.int3Z;
+		double raz = zc.z *fractal->transformCommon.int8Y / fractal->transformCommon.int3Z;
 
 		zc.x = zc.x - (fractal->transformCommon.offset1 * cos(ra) + fractal->transformCommon.offsetA2);
 		zc.y = zc.y - (fractal->transformCommon.offset1 * sin(raz) + fractal->transformCommon.offsetA2);
 
-		twist = sqrt(zc.x * zc.x +  zc.y * zc.y);
+		aux.DE0 = sqrt(zc.x * zc.x +  zc.y * zc.y) - fractal->transformCommon.offset01;
 
-		aux.DE0 = twist - fractal->transformCommon.offset01;
-
-	}
-
-	if (fractal->transformCommon.functionEnabledJFalse)
-	{
-		if (fractal->transformCommon.functionEnabledDFalse)
-			aux.DE0 = min(aux.dist, twist - fractal->transformCommon.offset01);
 		if (fractal->transformCommon.functionEnabledKFalse) aux.DE0 /= aux.DE;
-		if (fractal->transformCommon.functionEnabledEFalse) z = zc;
-	}
 
-	double colorDist = aux.dist;
-	aux.dist = aux.DE0;
+		if (!fractal->transformCommon.functionEnabledDFalse) aux.dist = min(aux.dist, aux.DE0);
+		else aux.dist = aux.DE0;
+	}
+	if (fractal->transformCommon.functionEnabledEFalse) z = zc;
 
 	// aux.color
 	if (fractal->foldColor.auxColorEnabled)
 	{
 		double colorAdd = 0.0;
-		double ang = (M_PI - 2.0 * fabs(atan(zc.x / zc.z))) * 2.0 / M_PI;
 
 		if (fmod(ang, 2.0) < 1.0) colorAdd += fractal->foldColor.difs0000.x;
-		colorAdd += fractal->foldColor.difs0000.y * fabs(ang);
-		colorAdd += fractal->foldColor.difs0000.z * fabs(ang * zc.x);
-		colorAdd += fractal->foldColor.difs0000.w * ang;
+		colorAdd += fractal->foldColor.difs0000.y * (zc.x);
+		colorAdd += fractal->foldColor.difs0000.z * (zc.y);
+		colorAdd += fractal->foldColor.difs0000.w * (zc.z);
 
 		colorAdd += fractal->foldColor.difs1;
 		if (fractal->foldColor.auxColorEnabledA)
