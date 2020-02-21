f0a91e26dbebc57fee65b1b0cae55ff12e9503a6
mclarekin <mclarekin@gmail.com>
2020-02-22 00:04:48 +1300

    testing2

--- mandelbulber2/formula/definition/fractal_testing_transform2.cpp
+++ mandelbulber2/formula/definition/fractal_testing_transform2.cpp
@@ -18,79 +18,35 @@ cFractalTestingTransform2::cFractalTestingTransform2() : cAbstractFractal()
 	internalName = "testing_transform2";
 	internalID = fractal::testingTransform2;
 	DEType = analyticDEType;
-	DEFunctionType = logarithmicDEFunction;
-	cpixelAddition = cpixelEnabledByDefault;
+	DEFunctionType = withoutDEFunction;
+	cpixelAddition = cpixelDisabledByDefault;
 	defaultBailout = 100.0;
-	DEAnalyticFunction = analyticFunctionLogarithmic;
+	DEAnalyticFunction = analyticFunctionNone;
 	coloringFunction = coloringFunctionDefault;
 }
 
 void cFractalTestingTransform2::FormulaCode(CVector4 &z, const sFractal *fractal, sExtendedAux &aux)
 {
-	// double colorAdd = 0.0;
-	aux.DE = 2.0 * aux.DE * aux.r + 1.0;
-
-	//  pre-abs
-	if (fractal->transformCommon.functionEnabledAxFalse
-			&& aux.i >= fractal->transformCommon.startIterationsA
-			&& aux.i < fractal->transformCommon.stopIterationsA)
+	CVector4 v;
+	switch (fractal->mandelbulbMulti.orderOfXYZ)
 	{
-		if (fractal->transformCommon.functionEnabledCxFalse) z.x = fabs(z.x);
-		if (fractal->transformCommon.functionEnabledCyFalse) z.y = fabs(z.y);
-		if (fractal->transformCommon.functionEnabledCzFalse) z.z = fabs(z.z);
+		case multi_OrderOfXYZ_xyz:
+		default: v = CVector4(z.x, z.y, z.z, z.w); break;
+		case multi_OrderOfXYZ_xzy: v = CVector4(z.x, z.z, z.y, z.w); break;
+		case multi_OrderOfXYZ_yxz: v = CVector4(z.y, z.x, z.z, z.w); break;
+		case multi_OrderOfXYZ_yzx: v = CVector4(z.y, z.z, z.x, z.w); break;
+		case multi_OrderOfXYZ_zxy: v = CVector4(z.z, z.x, z.y, z.w); break;
+		case multi_OrderOfXYZ_zyx: v = CVector4(z.z, z.y, z.x, z.w); break;
 	}
-
-	CVector4 ZZ = z * z;
-	double rr = z.Dot(z);
-	double theta = atan2(z.z, sqrt(ZZ.x + ZZ.y));
-	double phi = atan2(z.y, z.x);
-	double thetatemp = theta;
-
-	/*if(usespin)if(spin)
-		{
-			theta = -theta;
-	if(theta > 0.0)
-	{
-		theta = -pi1p5 + theta;
-	}
-	else
-	{
-		double alpha = pi0p5 + theta;
-		theta = M_PI + alpha;
-	}
-	theta = theta + M_PI;
-	}*/
-
-	double phi_pow = 2.0 * phi + M_PI;
-	double theta_pow = theta + M_PI + M_PI / 2.0; // piAdd;+ M_PI / 2.0
-	if (fractal->transformCommon.functionEnabledFalse) theta_pow = theta + M_PI / 4.0;
-	// theta_pow = theta + thetatemp + M_PI / 2.0;
-	if (fractal->transformCommon.functionEnabledAFalse) theta_pow = theta + thetatemp + M_PI;
-
-	double rn_sin_theta_pow = rr * sin(theta_pow);
-	z.x = rn_sin_theta_pow * cos(phi_pow); //  + jx
-	z.y = rn_sin_theta_pow * sin(phi_pow); // + jy
-	z.z = rr * cos(theta_pow);						 //  + jz
-
-	// z+=(Julia ? JuliaC : pos);
-
-	/*if(usespin)
-	{
-		if(spin)
-		{
-			theta += thetatemp - piAdd;
-					if(theta > pi1p5) spin = !spin;
-		else
-		{
-		if(theta < pi0p5) spin = !spin;
-		}
-		}
-		else
-	{
-			if(theta > pi0p25) spin = !spin;
-			 else if(theta < -pi0p25) spin = !spin;
-		}
-	}*/
+	if (fractal->transformCommon.functionEnabledAx)
+		z.x += (sin((v.x - fractal->transformCommon.offset000.x) * fractal->transformCommon.constantMultiplierA111.x)
+				* fractal->transformCommon.constantMultiplierB111.x);
+	if (fractal->transformCommon.functionEnabledAyFalse)
+		z.y += (sin((v.y - fractal->transformCommon.offset000.y) * fractal->transformCommon.constantMultiplierA111.y)
+				* fractal->transformCommon.constantMultiplierB111.y);
+	if (fractal->transformCommon.functionEnabledAzFalse)
+		z.z += (sin((v.z - fractal->transformCommon.offset000.z) * fractal->transformCommon.constantMultiplierA111.z)
+				* fractal->transformCommon.constantMultiplierB111.z);
 
 	// DE tweak
 	if (fractal->analyticDE.enabledFalse)
