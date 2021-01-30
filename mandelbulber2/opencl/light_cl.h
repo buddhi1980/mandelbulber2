6d922249252a4a5e700ae284d13d00b24c4f9948
buddhi1980 <buddhi1980@gmail.com>
2021-01-30 12:17:46 +0100

    added missing light_cl.h

---
+++ mandelbulber2/opencl/light_cl.h
@@ -0,0 +1,65 @@
+/*
+ * light_cl.h
+ *
+ *  Created on: 30 sty 2021
+ *      Author: krzysztof
+ */
+
+#ifndef MANDELBULBER2_OPENCL_LIGHT_CL_H_
+#define MANDELBULBER2_OPENCL_LIGHT_CL_H_
+
+#ifndef OPENCL_KERNEL_CODE
+#include "opencl_algebra.h"
+#endif /* OPENCL_KERNEL_CODE */
+
+typedef enum
+{
+	lightGlobal = 0,
+	lightSpot = 1,
+	lightConical = 2,
+	lightProjection = 3
+} enumLightTypeCl;
+
+typedef enum
+{
+	lightDecay1R = 0,
+	lightDecay1R2 = 1,
+	lightDecay1R3 = 2,
+} enumLightDecayFunctionCl;
+
+typedef struct
+{
+	// booleans
+	cl_int enabled;
+	cl_int castShadows;
+	cl_int penetrating;
+	cl_int relativePosition;
+	cl_int volumetric;
+
+	// enums
+	enumLightTypeCl type;
+	enumLightDecayFunctionCl decayFunction;
+
+	cl_float coneAngle;
+	cl_float coneSoftAngle;
+	cl_float intensity;
+	cl_float visibility;
+	cl_float volumetricVisibility;
+	cl_float size;
+	cl_float softShadowCone;
+	cl_float contourSharpness;
+
+	cl_float coneRatio;
+	cl_float coneSoftRatio;
+
+	cl_float3 position;
+	cl_float3 rotation;
+	cl_float3 lightDirection;
+
+	cl_float3 color;
+
+	matrix33 rotMatrix;
+
+} sLightCl;
+
+#endif /* MANDELBULBER2_OPENCL_LIGHT_CL_H_ */
