d4236d251888e34e1ee1c5455f8145c8e81b3b16
mclarekin <mclarekin@gmail.com>
2023-01-11 00:49:39 +1300

    pow2 v1 test

--- mandelbulber2/formula/definition/fractal_mandelbulb_pow2_v1.cpp
+++ mandelbulber2/formula/definition/fractal_mandelbulb_pow2_v1.cpp
@@ -27,29 +27,81 @@ cFractalMandelbulbPow2V1::cFractalMandelbulbPow2V1() : cAbstractFractal()
 
 void cFractalMandelbulbPow2V1::FormulaCode(CVector4 &z, const sFractal *fractal, sExtendedAux &aux)
 {
-
-	double m2 = z.y * z.y + z.x * z.x;
-	aux.DE = aux.DE * 2.0 * sqrt(m2 + z.z * z.z) + 1.0;
-	double temp = m2 - z.z * z.z;
-
-	z.z = 2.0 * sqrt(m2) * z.z;
-	if (fractal->transformCommon.functionEnabledAFalse && z.x < 0.0) z.z = -z.z;
-	if (fractal->transformCommon.functionEnabledBFalse && z.z < 0.0) z.x = -z.x;
-
-	m2 = temp / m2;
-	temp = 2.0 * z.y * z.x * m2;
-	z.y = (z.y * z.y - z.x * z.x) * m2;
-
-	if (!fractal->transformCommon.functionEnabledCFalse)
-		z.x = temp;
-	else
-		z.x = -temp;
-
-	if (fractal->transformCommon.functionEnabledDFalse)
+	if (fractal->transformCommon.functionEnabledAxFalse
+			&& aux.i >= fractal->transformCommon.startIterationsA
+			&& aux.i < fractal->transformCommon.stopIterationsA)
 	{
-		z.z *= aux.pos_neg;
-		aux.pos_neg *= -1.0;
+		if (fractal->transformCommon.functionEnabledCxFalse) z.x = fabs(z.x);
+		if (fractal->transformCommon.functionEnabledCyFalse) z.y = fabs(z.y);
+		if (fractal->transformCommon.functionEnabledCzFalse) z.z = fabs(z.z);
 	}
+
+	if (fractal->transformCommon.functionEnabledAyFalse
+			&& aux.i >= fractal->transformCommon.startIterationsB
+			&& aux.i < fractal->transformCommon.stopIterationsB)
+	{
+
+
+		double m2 = z.y * z.y + z.x * z.x;
+		aux.DE = aux.DE * 2.0 * sqrt(m2 + z.z * z.z) + 1.0;
+
+		if (m2 == 0.0f)
+		{
+			z.y = -z.z * z.z;
+			z.z = 0.0f;
+		}
+		else
+		{
+			double temp = m2 - z.z * z.z;
+
+			z.z = 2.0 * sqrt(m2) * z.z;
+		//	if (fractal->transformCommon.functionEnabledAFalse && z.x < 0.0) z.z = -z.z;
+		//	if (fractal->transformCommon.functionEnabledBFalse && z.z < 0.0) z.x = -z.x;
+
+			m2 = temp / m2;
+			temp = 2.0 * z.y * z.x * m2;
+			z.y = (z.y * z.y - z.x * z.x) * m2;
+
+			if (!fractal->transformCommon.functionEnabledAFalse)
+				z.x = temp;
+			else
+				z.x = -temp;
+
+			if (fractal->transformCommon.functionEnabledDFalse)
+			{
+				z.z *= aux.pos_neg;
+				aux.pos_neg *= -1.0;
+			}
+		}
+	}
+
+	if (fractal->transformCommon.functionEnabledAzFalse
+			&& aux.i >= fractal->transformCommon.startIterationsC
+			&& aux.i < fractal->transformCommon.stopIterationsC)
+	{
+
+		double m2 = z.x * z.x + z.z * z.z;
+		aux.DE = aux.DE * 2.0 * sqrt(m2 + z.y * z.y) + 1.0;
+		double temp = -(m2 - z.y * z.y);
+
+		z.y = 2.0 * sqrt(m2) * z.y;
+		//if (fractal->transformCommon.functionEnabledAFalse && z.y < 0.0) z.x = -z.x;
+		//if (fractal->transformCommon.functionEnabledBFalse && z.x < 0.0) z.y = -z.y;
+		m2 = temp / m2;
+		temp = 2.0 * z.x * z.z * m2;
+		z.z = (z.z * z.z - z.x * z.x) * m2;
+		if (!fractal->transformCommon.functionEnabledBFalse)
+			z.x = temp;
+		else
+			z.x = -temp;
+		if (fractal->transformCommon.functionEnabledDFalse)
+		{
+			z.y *= aux.pos_neg;
+			aux.pos_neg *= -1.0f;
+		}
+	}
+
+
 
 	// Analytic DE tweak
 	if (fractal->analyticDE.enabledFalse)
--- mandelbulber2/formula/opencl/mandelbulb_pow2_v1.cl
+++ mandelbulber2/formula/opencl/mandelbulb_pow2_v1.cl
@@ -18,28 +18,75 @@
 REAL4 MandelbulbPow2V1Iteration(REAL4 z, __constant sFractalCl *fractal, sExtendedAuxCl *aux)
 {
 
-	REAL m2 = z.x * z.x + z.y * z.y;
-	aux->DE = aux->DE * 2.0f * sqrt(m2 + z.z * z.z) + 1.0f;
-	REAL temp = m2 - z.z * z.z;
-
-	z.z = 2.0 * sqrt(m2) * z.z;
-	if (fractal->transformCommon.functionEnabledAFalse && z.y < 0.0f) z.x = -z.x;
-	if (fractal->transformCommon.functionEnabledBFalse && z.x < 0.0f) z.y = -z.y;
-
-	m2 = temp / m2;
-	temp = 2.0 * z.x * z.y * m2;
-	z.x = (z.x * z.x - z.y * z.y) * m2;
-
-	if (!fractal->transformCommon.functionEnabledCFalse)
-		z.y = temp;
-	else
-		z.y = -temp;
-
-	if (fractal->transformCommon.functionEnabledDFalse)
+	if (fractal->transformCommon.functionEnabledAxFalse
+			&& aux->i >= fractal->transformCommon.startIterationsA
+			&& aux->i < fractal->transformCommon.stopIterationsA)
 	{
-		z.y *= aux->pos_neg;
-		aux->pos_neg *= -1.0f;
+		if (fractal->transformCommon.functionEnabledCxFalse) z.x = fabs(z.x);
+		if (fractal->transformCommon.functionEnabledCyFalse) z.y = fabs(z.y);
+		if (fractal->transformCommon.functionEnabledCzFalse) z.z = fabs(z.z);
 	}
+
+	if (fractal->transformCommon.functionEnabledAyFalse
+			&& aux->i >= fractal->transformCommon.startIterationsB
+			&& aux->i < fractal->transformCommon.stopIterationsB)
+	{
+
+		REAL m2 = z.x * z.x + z.y * z.y;
+		aux->DE = aux->DE * 2.0f * sqrt(m2 + z.z * z.z) + 1.0f;
+		if (m2 == 0.0f)
+		{
+			z.y = -z.z * z.z;
+			z.z = 0.0f;
+		}
+		else
+		{
+			REAL temp = m2 - z.z * z.z;
+			z.z = 2.0f * sqrt(m2) * z.z;
+			//if (fractal->transformCommon.functionEnabledAFalse && z.x < 0.0) z.z = -z.z;
+			//if (fractal->transformCommon.functionEnabledBFalse && z.z < 0.0) z.x = -z.x;
+			m2 = temp / m2;
+			temp = 2.0f * z.x * z.y * m2;
+			z.y = (z.y * z.y - z.x * z.x) * m2;
+			if (!fractal->transformCommon.functionEnabledAFalse)
+				z.x = temp;
+			else
+				z.x = -temp;
+			if (fractal->transformCommon.functionEnabledDFalse)
+			{
+				z.z *= aux->pos_neg;
+				aux->pos_neg *= -1.0f;
+			}
+		}
+	}
+
+	if (fractal->transformCommon.functionEnabledAzFalse
+			&& aux->i >= fractal->transformCommon.startIterationsC
+			&& aux->i < fractal->transformCommon.stopIterationsC)
+	{
+		REAL m2 = z.x * z.x + z.z * z.z;
+		aux->DE = aux->DE * 2.0f * sqrt(m2 + z.y * z.y) + 1.0f;
+		REAL temp = -(m2 - z.y * z.y);
+
+		z.y = 2.0f * sqrt(m2) * z.y;
+		//if (fractal->transformCommon.functionEnabledAFalse && z.y < 0.0f) z.x = -z.x;
+		//if (fractal->transformCommon.functionEnabledBFalse && z.x < 0.0f) z.y = -z.y;
+		m2 = temp / m2;
+		temp = 2.0f * z.x * z.z * m2;
+		z.z = (z.z * z.z - z.x * z.x) * m2;
+		if (!fractal->transformCommon.functionEnabledBFalse)
+			z.x = temp;
+		else
+			z.x = -temp;
+		if (fractal->transformCommon.functionEnabledDFalse)
+		{
+			z.y *= aux->pos_neg;
+			aux->pos_neg *= -1.0f;
+		}
+	}
+
+
+
 
 	// Analytic DE tweak
 	if (fractal->analyticDE.enabledFalse)
