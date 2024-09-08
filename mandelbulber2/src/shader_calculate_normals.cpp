71b54814066151a719e16a5cbde9f1fe95b1ef3f
buddhi1980 <buddhi1980@gmail.com>
2024-09-08 17:22:22 +0200

    Fixed error in normal vector calculation

--- mandelbulber2/src/shader_calculate_normals.cpp
+++ mandelbulber2/src/shader_calculate_normals.cpp
@@ -107,7 +107,7 @@ CVector3 cRenderWorker::CalculateNormals(const sShaderInputData &input) const
 		}
 	}
 
-	if (fabs(normal.x) < 1e50 && fabs(normal.y) < 1e-50 && fabs(normal.z) < 1e-50)
+	if (fabs(normal.x) < 1e-50 && fabs(normal.y) < 1e-50 && fabs(normal.z) < 1e-50)
 	{
 		normal = CVector3(1.0, 0.0, 0.0);
 	}
