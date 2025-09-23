da690f3e87694a0d7818a87317ca268d5ea8ced3
buddhi1980 <buddhi1980@gmail.com>
2025-09-23 17:58:33 +0200

    Added examples for nebula fractals

--- mandelbulber2/qt/thumbnail_widget.cpp
+++ mandelbulber2/qt/thumbnail_widget.cpp
@@ -279,7 +279,7 @@ void cThumbnailWidget::AssignParameters(std::shared_ptr<const cParameterContaine
 					// render thumbnail after random time. It forces rendering of widgets when they are not
 					// visible. It makes rendering of widgets when they are idle.
 
-					timer->start(Random(3600000) + 30000); // one hour for all thumbnails
+					timer->start(Random(360000) + 3000);
 				}
 			}
 		}
@@ -312,7 +312,7 @@ void cThumbnailWidget::slotRender()
 			// try again after some random time
 			if (visibleRegion().isEmpty())
 			{
-				timer->start(Random(600000) + 1);
+				timer->start(Random(60000) + 1);
 			}
 			else
 			{
