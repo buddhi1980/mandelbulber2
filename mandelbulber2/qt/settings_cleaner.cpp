ecbb27768b8bee962a07cdac8e34c2587b2c36b1
buddhi1980 <buddhi1980@gmail.com>
2026-08-06 15:59:03 +0200

    settings_cleaner: skip detail_level and node definition parameters
    Add detail_level to the list of parameters that should not be cleaned.
    Also skip any parameter containing both "node" and "definition" to
    prevent removal of object tree node definition settings.

--- mandelbulber2/qt/settings_cleaner.cpp
+++ mandelbulber2/qt/settings_cleaner.cpp
@@ -144,6 +144,7 @@ void cSettingsCleaner::runCleaner()
 			if (parameterName == "image_width") continue;
 			if (parameterName == "image_height") continue;
 			if (parameterName == "N") continue;
+			if (parameterName == "detail_level") continue;
 			if (parameterName == "reflections_max") continue;
 			if (parameterName == "DOF_samples") continue;
 			if (parameterName == "DOF_min_samples") continue;
@@ -171,6 +172,7 @@ void cSettingsCleaner::runCleaner()
 			if (parameterName.contains("mat") && parameterName.contains("use_colors_from_palette"))
 				continue;
 			if (parameterName.contains("light") && parameterName.contains("is_defined")) continue;
+			if (parameterName.contains("node") && parameterName.contains("definition")) continue;
 
 			sDefaultedParameter par;
 			par.parameterName = parameterName;
