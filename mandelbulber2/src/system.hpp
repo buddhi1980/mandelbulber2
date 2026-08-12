1f79ac45d8d45342493f5f0010223d686d661770
buddhi1980 <buddhi1980@gmail.com>
2026-08-12 11:22:58 +0200

    fix: remove _HAS_STD_BYTE 0 define in system.hpp
    Qt 6.11 requires _HAS_STD_BYTE=1. The define in system.hpp conflicted
    with the vcxproj preprocessor definition, causing Qt build error:
    "Qt requires std::byte, but _HAS_STD_BYTE has been set to 0"

--- mandelbulber2/src/system.hpp
+++ mandelbulber2/src/system.hpp
@@ -56,7 +56,6 @@
 #ifdef _WIN32 /* WINDOWS */
 // #include <windows.h>
 #define HAVE_BOOLEAN /* prevent jmorecfg.h from redefining it */
-#define _HAS_STD_BYTE 0 /* prevent std::byte from conflicting with Windows SDK byte */
 #endif
 
 #ifndef SHARED_DIR
