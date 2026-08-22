8a93d091cf1f45c7f8c0cd5f59b48a3f24fa6561
Coastrnaut <rpancoast77@gmail.com>
2026-08-22 14:07:15 -0400

    Update abstract_fractal.cpp

--- mandelbulber2/formula/definition/abstract_fractal.cpp
+++ mandelbulber2/formula/definition/abstract_fractal.cpp
@@ -31,47 +31,47 @@ bool cAbstractFractal::CheckForErrors() const
 	bool error = false;
 	if (nameInComboBox == "")
 	{
-		qCritical() << "Undefined nameInComboBox in formula " << internalName;
+		qCritical() << "Undefined nameInComboBox in formula " << QString::fromStdString(internalName);
 		error = true;
 	}
 	if (internalName == "")
 	{
-		qCritical() << "Undefined internalName in formula " << nameInComboBox;
+		qCritical() << "Undefined internalName in formula " << QString::fromStdString(nameInComboBox);
 		error = true;
 	}
 	if (internalID == -1)
 	{
-		qCritical() << "Undefined internalID in formula " << internalName;
+		qCritical() << "Undefined internalID in formula " << QString::fromStdString(internalName);
 		error = true;
 	}
 	if (DEType == undefinedDEType)
 	{
-		qCritical() << "Undefined DEType in formula " << internalName;
+		qCritical() << "Undefined DEType in formula " << QString::fromStdString(internalName);
 		error = true;
 	}
 	if (DEFunctionType == undefinedDEFunction)
 	{
-		qCritical() << "Undefined DEFunctionType in formula " << internalName;
+		qCritical() << "Undefined DEFunctionType in formula " << QString::fromStdString(internalName);
 		error = true;
 	}
 	if (cpixelAddition == cpixelUndefined)
 	{
-		qCritical() << "Undefined cpixelAddition in formula " << internalName;
+		qCritical() << "Undefined cpixelAddition in formula " << QString::fromStdString(internalName);
 		error = true;
 	}
 	if (defaultBailout == -1.0)
 	{
-		qCritical() << "Undefined defaultBailout in formula " << internalName;
+		qCritical() << "Undefined defaultBailout in formula " << QString::fromStdString(internalName);
 		error = true;
 	}
 	if (DEAnalyticFunction == analyticFunctionUndefined)
 	{
-		qCritical() << "Undefined DEAnalyticFunction in formula " << internalName;
+		qCritical() << "Undefined DEAnalyticFunction in formula " << QString::fromStdString(internalName);
 		error = true;
 	}
 	if (coloringFunction == coloringFunctionUndefined)
 	{
-		qCritical() << "Undefined coloringFunction in formula " << internalName;
+		qCritical() << "Undefined coloringFunction in formula " << QString::fromStdString(internalName);
 		error = true;
 	}
 
